3df24bab433dacd57bc405e2852b0eedd87ea1b4
2HYUNHO <soda9503@gmail.com>
2024-12-30 16:25:46 +0900

    [Feat] 프로필 설정 뷰 완료
    - 카메라 버튼 클릭 시 팝업 생성
    - 앨범에서 사진 선택 후 프로필 사진 반영

--- Where/Views/ProfileCreationView.swift
+++ Where/Views/ProfileCreationView.swift
@@ -11,105 +11,118 @@ struct ProfileCreationView: View {
     @Environment(\.dismiss) private var dismiss
     @State var username: String = ""
     @State private var isValid: Bool = false
+    @State private var showPopup = false
+    @State private var profileImage: UIImage? = UIImage(named: "person")
     
     var body: some View {
         NavigationStack {
-            VStack {
-                HStack {
-                    Text("프로필을 설정해주세요")
-                        .font(.system(size: 24.0))
-                        .fontWeight(.bold)
-                    Spacer()
-                }
-                .padding(.top, 40)
-                .padding(.horizontal)
-                
-                ZStack(alignment: .bottomTrailing) {
-                    Image("person")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 155, height: 155)
-                    
-                    Button {
-                     
-                    } label: {
-                        Image("CameraButton")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 40, height: 40)
+            ZStack {
+                VStack {
+                    HStack {
+                        Text("프로필을 설정해주세요")
+                            .font(.system(size: 24.0))
+                            .fontWeight(.bold)
+                        Spacer()
                     }
-                    .buttonStyle(PlainButtonStyle())
-                }
-                .padding(.top, 58)
-                
-                HStack {
-                    Text("닉네임")
-                    Spacer()
-                }
-                .padding(.top, 38)
-                .padding(.horizontal)
-                
-                ZStack(alignment: .trailing) {
-                    TextField("",
-                              text: $username,
-                              prompt: Text("닉네임을 입력해주세요").foregroundStyle(Color(hex: 0x6B7280))
-                    )
-                    .frame(height: 56)
-                    .textFieldStyle(PlainTextFieldStyle())
-                    .padding([.horizontal], 15)
-                    .overlay(RoundedRectangle(cornerRadius: 12).stroke(isValid ? Color.green : Color(hex: 0xE5E7EB)))
+                    .padding(.top, 40)
                     .padding(.horizontal)
-                    .onChange(of: username) { newValue in
-                        // 텍스트 필드가 비어 있으면 isValid를 false로 설정
-                        if newValue.isEmpty {
-                            isValid = false
-                        } else {
-                            // 8자 이상 입력 제한
-                            let maxLength = 8
-                            let filteredValue = newValue.filter { $0.isLetter || $0.isNumber || $0 == "-" || $0 == "_" }
-                            
-                            // 8자 이상일 경우 8자까지만 잘라내기
-                            username = String(filteredValue.prefix(maxLength))
-                            
-                            // 유효성 검사: 특수문자, 이모지 제외
-                            isValid = username.count <= maxLength && !username.containsEmoji && username.allSatisfy {
-                                $0.isLetter || $0.isNumber || $0 == "-" || $0 == "_"
+                    
+                    ZStack(alignment: .bottomTrailing) {
+                        if let image = profileImage {
+                            Image(uiImage: image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 155, height: 155)
+                                .clipShape(Circle())
+                        }
+                        Button {
+                            withAnimation {
+                                showPopup = true
                             }
+                        } label: {
+                            Image("CameraButton")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 40, height: 40)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                    .padding(.top, 58)
+                    
+                    HStack {
+                        Text("닉네임")
+                        Spacer()
+                    }
+                    .padding(.top, 38)
+                    .padding(.horizontal)
+                    
+                    ZStack(alignment: .trailing) {
+                        // 팝업창
+                        TextField("",
+                                  text: $username,
+                                  prompt: Text("닉네임을 입력해주세요").foregroundStyle(Color(hex: 0x6B7280))
+                        )
+                        .frame(height: 56)
+                        .textFieldStyle(PlainTextFieldStyle())
+                        .padding([.horizontal], 15)
+                        .overlay(RoundedRectangle(cornerRadius: 12).stroke(isValid ? Color.green : Color(hex: 0xE5E7EB)))
+                        .padding(.horizontal)
+                        .onChange(of: username) { newValue in
+                            // 텍스트 필드가 비어 있으면 isValid를 false로 설정
+                            if newValue.isEmpty {
+                                isValid = false
+                            } else {
+                                // 8자 이상 입력 제한
+                                let maxLength = 8
+                                let filteredValue = newValue.filter { $0.isLetter || $0.isNumber || $0 == "-" || $0 == "_" }
+                                
+                                // 8자 이상일 경우 8자까지만 잘라내기
+                                username = String(filteredValue.prefix(maxLength))
+                                
+                                // 유효성 검사: 특수문자, 이모지 제외
+                                isValid = username.count <= maxLength && !username.containsEmoji && username.allSatisfy {
+                                    $0.isLetter || $0.isNumber || $0 == "-" || $0 == "_"
+                                }
+                            }
+                        }
+                        
+                        // 체크 아이콘
+                        if isValid {
+                            Image(systemName: "checkmark")
+                                .foregroundColor(.green)
+                                .padding(.trailing, 30)
                         }
                     }
                     
-                    // 체크 아이콘
-                    if isValid {
-                        Image(systemName: "checkmark")
-                            .foregroundColor(.green)
-                            .padding(.trailing, 30)
+                    
+                    HStack {
+                        Text(isValid ? "사용 가능한 닉네임입니다" : "8자 내, 이모지, 특수문자(-,_제외)를 사용할 수 없습니다.")
+                            .font(.system(size: 14))
+                            .foregroundColor(isValid ? .green : .black)  // 유효성에 따라 색상 변경
+                            .padding(.horizontal)
+                        
+                        Spacer()
                     }
-                }
-          
-                
-                HStack {
-                    Text(isValid ? "사용 가능한 닉네임입니다" : "8자 내, 이모지, 특수문자(-,_제외)를 사용할 수 없습니다.")
-                        .font(.system(size: 14))
-                        .foregroundColor(isValid ? .green : .black)  // 유효성에 따라 색상 변경
-                        .padding(.horizontal)
                     
                     Spacer()
+                    
+                    Button {
+                        // 버튼 클릭 시 동작할 코드
+                    } label: {
+                        Text("다음")
+                            .frame(maxWidth: .infinity)
+                            .padding()  // 버튼 내부 여백
+                            .background(Color(hex: 0x4F46E5))  // 배경색 설정
+                            .foregroundStyle(.white)  // 텍스트 색을 흰색으로 설정
+                            .bold()
+                            .cornerRadius(10)  // 둥근 모서리
+                    }
+                    .padding()
                 }
                 
-                Spacer()
-                
-                Button {
-                    // 버튼 클릭 시 동작할 코드
-                } label: {
-                    Text("다음")
-                        .frame(maxWidth: .infinity)
-                        .padding()  // 버튼 내부 여백
-                        .background(Color(hex: 0x4F46E5))  // 배경색 설정
-                        .foregroundStyle(.white)  // 텍스트 색을 흰색으로 설정
-                        .bold()
-                        .cornerRadius(10)  // 둥근 모서리
+                if showPopup {
+                    ProfilePopupView(showPopup: $showPopup, profileImage: $profileImage)
                 }
-                .padding()
             }
             .toolbar {
                 ToolbarItem(placement: .topBarLeading) {
@@ -125,6 +138,7 @@ struct ProfileCreationView: View {
         }
     }
 }
+
 
 #Preview {
     ProfileCreationView()
