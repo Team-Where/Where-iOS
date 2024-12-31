43d3c04da04e51746b25cf9fad3944949df04a6f
YunSwain <destap@naver.com>
2024-12-31 14:12:58 +0900

    [Refactor]: disabled 상태에 따라 style 바뀔 수 있도록 수정

--- Where/Utility/DesignSystem/Components/RoundedProminentButtonStyle.swift
+++ Where/Utility/DesignSystem/Components/RoundedProminentButtonStyle.swift
@@ -8,15 +8,18 @@
 import SwiftUI
 
 struct RoundedProminentButtonStyle: ButtonStyle {
+    let disabled: Bool
     let foregroundColor: Color
     let backgroundColor: Color
     
     init(
+        _ disabled: Bool,
         _ foregroundColor: Color,
         _ backgroundColor: Color
     ) {
         self.foregroundColor = foregroundColor
         self.backgroundColor = backgroundColor
+        self.disabled = disabled
     }
     
     func makeBody(configuration: Configuration) -> some View {
@@ -26,16 +29,17 @@ struct RoundedProminentButtonStyle: ButtonStyle {
             .foregroundStyle(foregroundColor)
             .background(
                 RoundedRectangle(cornerRadius: 10)
-                    .fill(backgroundColor)
+                    .fill(disabled ? Color(hex: 0xDEE2E6) : backgroundColor)
             )
     }
 }
 
 extension ButtonStyle where Self == RoundedProminentButtonStyle {
     static func whereRoundedProminent(
+        _ disabled: Bool = false,
         _ foreground: Color = .white,
         _ background: Color = .accent
     ) -> RoundedProminentButtonStyle {
-        RoundedProminentButtonStyle(foreground, background)
+        RoundedProminentButtonStyle(disabled, foreground, background)
     }
 }
