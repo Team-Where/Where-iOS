929c10aa3de0adadc2aa01b1cd7646cef41eb5b1
2HYUNHO <soda9503@gmail.com>
2025-01-02 22:21:55 +0900

    [Docs] 주석 수정

--- Where/Utility/String.swift
+++ Where/Utility/String.swift
@@ -28,19 +28,19 @@ extension Character {
 extension UnicodeScalar {
     // 이모지 여부를 확인
     var isEmoji: Bool {
-        return (self.value >= 0x1F600 && self.value <= 0x1F64F) || // Emoticons
-               (self.value >= 0x1F300 && self.value <= 0x1F5FF) || // Miscellaneous Symbols and Pictographs
-               (self.value >= 0x1F680 && self.value <= 0x1F6FF) || // Transport and Map Symbols
-               (self.value >= 0x1F700 && self.value <= 0x1F77F) || // Alchemical Symbols
-               (self.value >= 0x1F780 && self.value <= 0x1F7FF) || // Geometric Shapes Extended
-               (self.value >= 0x1F800 && self.value <= 0x1F8FF) || // Supplemental Symbols and Pictographs
-               (self.value >= 0x1F900 && self.value <= 0x1F9FF) || // Supplemental Symbols and Pictographs
-               (self.value >= 0x1FA00 && self.value <= 0x1FA6F) || // Chess Symbols
-               (self.value >= 0x1FA70 && self.value <= 0x1FAFF) || // Symbols and Pictographs Extended
-               (self.value >= 0x2600 && self.value <= 0x26FF) ||   // Miscellaneous Symbols
-               (self.value >= 0x2700 && self.value <= 0x27BF) ||   // Dingbats
-               (self.value == 0x2B50) ||                         // Star
-               (self.value == 0x2B06) ||                         // Upwards Button
-               (self.value == 0x2B07)                           // Downwards Button
+        return (self.value >= 0x1F600 && self.value <= 0x1F64F) || // 이모티콘
+               (self.value >= 0x1F300 && self.value <= 0x1F5FF) || // 기타 기호 및 그림문자
+               (self.value >= 0x1F680 && self.value <= 0x1F6FF) || // 교통 및 지도 기호
+               (self.value >= 0x1F700 && self.value <= 0x1F77F) || // 연금술 기호
+               (self.value >= 0x1F780 && self.value <= 0x1F7FF) || // 확장된 기하학적 도형
+               (self.value >= 0x1F800 && self.value <= 0x1F8FF) || // 보충 기호 및 그림문자
+               (self.value >= 0x1F900 && self.value <= 0x1F9FF) || // 보충 기호 및 그림문자
+               (self.value >= 0x1FA00 && self.value <= 0x1FA6F) || // 체스 기호
+               (self.value >= 0x1FA70 && self.value <= 0x1FAFF) || // 확장된 기호 및 그림문자
+               (self.value >= 0x2600 && self.value <= 0x26FF) ||   // 기타 기호
+               (self.value >= 0x2700 && self.value <= 0x27BF) ||   // 딩뱃 기호
+               (self.value == 0x2B50) ||                         // 별 모양
+               (self.value == 0x2B06) ||                         // 위쪽 화살표 버튼
+               (self.value == 0x2B07)                           // 아래쪽 화살표 버튼
     }
 }
