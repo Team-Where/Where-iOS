6ee0282d079737798818f5af321128ac2bd97e0d
YunSwain <destap@naver.com>
2025-01-03 15:40:05 +0900

    [Feat]: AppStorageKey 네임스페이스 추가

---
+++ Where/Utility/AppStorageKey.swift
@@ -0,0 +1,13 @@
+//
+//  AppStorageKey.swift
+//  Where
+//
+//  Created by Swain Yun on 1/3/25.
+//
+
+import Foundation
+
+/// AppStorageKey로 사용되는 문자열
+struct AppStorageKey {
+    static let isOnboardingNeeded = "isOnboardingNeeded"
+}
