4a40cfd7d88383d248808a9df76b281849567cac
YunSwain <destap@naver.com>
2025-01-04 17:39:43 +0900

    [Docs]: 12시간제 표시 주석 수정

--- Where/Utility/DateFormat.swift
+++ Where/Utility/DateFormat.swift
@@ -42,8 +42,8 @@ enum DateFormat: String {
     /// 년.월.일 시:분
     case yyyyMMddHHmm = "yyyy.MM.dd HH:mm"
     
-    /// 년.월.일 오전/오후 시
-    case yyyyMMddahh = "yyyy.MM.dd a hh시"
+    /// 년.월.일 오전/오후 시(12)
+    case yyyyMMddah = "yyyy.MM.dd a h시"
     
     /// 년.월.일 오전/오후 시:분
     case yyyyMMddahhmm = "yyyy.MM.dd a hh:mm"
