783b73484d4036bea67668e8498b5e99619b39dd
YunSwain <destap@naver.com>
2025-01-23 17:31:19 +0900

    [Feat]: 소셜 로그인 공급자 구분

--- Where/Models/AuthentificationProvider.swift
+++ Where/Models/AuthentificationProvider.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+/// O-Auth 공급자 종류
+enum AuthentificationProvider {
+    case apple
+    case kakao
+    case naver
+    
+    /// Bundle 검색에 필요한 키
+    var infoDictionaryKey: String {
+        switch self {
+        case .apple: "APPLE_NATIVE_APP_KEY"
+        case .kakao: "KAKAO_NATIVE_APP_KEY"
+        case .naver: "NAVER_NATIVE_APP_KEY"
+        }
+    }
+}
