da17958f783153c12012f5d609a9956d6619e9ab
YunSwain <destap@naver.com>
2025-01-28 17:48:19 +0900

    Revert "[Refactor]: 필요한 색상이 사용시점에 초기화 될 수 있도록 수정"
    This reverts commit d3cb0a207dd50ac23ce321f19e747da5e266e089.

--- Where/Utility/Extensions/Color Extension.swift
+++ Where/Utility/Extensions/Color Extension.swift
@@ -26,154 +26,155 @@ extension ShapeStyle where Self == Color {
 struct WhereColor {
     // Neutral Colors
     /// 0xF9FAFB
-    lazy var gray50: Color = NeutralHex.gray50.color()
+    let gray50: Color = NeutralHex.gray50.color()
     /// 0xF3F4F6
-    lazy var gray100: Color = NeutralHex.gray100.color()
+    let gray100: Color = NeutralHex.gray100.color()
     /// 0xE5E7EB
-    lazy var gray200: Color = NeutralHex.gray200.color()
+    let gray200: Color = NeutralHex.gray200.color()
     /// 0xD1D5DB
-    lazy var gray300: Color = NeutralHex.gray300.color()
+    let gray300: Color = NeutralHex.gray300.color()
     /// 0x9CA3AF
-    lazy var gray400: Color = NeutralHex.gray400.color()
+    let gray400: Color = NeutralHex.gray400.color()
     /// 0x6B7280
-    lazy var gray500: Color = NeutralHex.gray500.color()
+    let gray500: Color = NeutralHex.gray500.color()
     /// 0x4B5563
-    lazy var gray600: Color = NeutralHex.gray600.color()
+    let gray600: Color = NeutralHex.gray600.color()
     /// 0x374151
-    lazy var gray700: Color = NeutralHex.gray700.color()
+    let gray700: Color = NeutralHex.gray700.color()
     /// 0x1F2937
-    lazy var gray800: Color = NeutralHex.gray800.color()
+    let gray800: Color = NeutralHex.gray800.color()
     /// 0x111827
-    lazy var gray900: Color = NeutralHex.gray900.color()
+    let gray900: Color = NeutralHex.gray900.color()
     
     // Primary Colors
     /// 0xEEF2FF
-    lazy var indigo50: Color = PrimaryHex.indigo50.color()
+    let indigo50: Color = PrimaryHex.indigo50.color()
     /// 0xE0E7FF
-    lazy var indigo100: Color = PrimaryHex.indigo100.color()
+    let indigo100: Color = PrimaryHex.indigo100.color()
     /// 0xC7D2FE
-    lazy var indigo200: Color = PrimaryHex.indigo200.color()
+    let indigo200: Color = PrimaryHex.indigo200.color()
     /// 0xA5B4FC
-    lazy var indigo300: Color = PrimaryHex.indigo300.color()
+    let indigo300: Color = PrimaryHex.indigo300.color()
     /// 0x818CF8
-    lazy var indigo400: Color = PrimaryHex.indigo400.color()
+    let indigo400: Color = PrimaryHex.indigo400.color()
     /// 0x6366F1
-    lazy var indigo500: Color = PrimaryHex.indigo500.color()
+    let indigo500: Color = PrimaryHex.indigo500.color()
     /// 0x4F46E5
-    lazy var indigo600: Color = PrimaryHex.indigo600.color()
+    let indigo600: Color = PrimaryHex.indigo600.color()
     /// 0x4338CA
-    lazy var indigo700: Color = PrimaryHex.indigo700.color()
+    let indigo700: Color = PrimaryHex.indigo700.color()
     /// 0x3730A3
-    lazy var indigo800: Color = PrimaryHex.indigo800.color()
+    let indigo800: Color = PrimaryHex.indigo800.color()
     /// 0x312E81
-    lazy var indigo900: Color = PrimaryHex.indigo900.color()
+    let indigo900: Color = PrimaryHex.indigo900.color()
 
     // Semantic Colors
     /// 0xFEF2F2
-    lazy var red50: Color = SemanticHex.red50.color()
+    let red50: Color = SemanticHex.red50.color()
     /// 0xFEE2E2
-    lazy var red100: Color = SemanticHex.red100.color()
+    let red100: Color = SemanticHex.red100.color()
     /// 0xFECACA
-    lazy var red200: Color = SemanticHex.red200.color()
+    let red200: Color = SemanticHex.red200.color()
     /// 0xFCA5A5
-    lazy var red300: Color = SemanticHex.red300.color()
+    let red300: Color = SemanticHex.red300.color()
     /// 0xF87171
-    lazy var red400: Color = SemanticHex.red400.color()
+    let red400: Color = SemanticHex.red400.color()
     /// 0xEF4444
-    lazy var red500: Color = SemanticHex.red500.color()
+    let red500: Color = SemanticHex.red500.color()
     /// 0xDC2626
-    lazy var red600: Color = SemanticHex.red600.color()
+    let red600: Color = SemanticHex.red600.color()
     /// 0xB91C1C
-    lazy var red700: Color = SemanticHex.red700.color()
+    let red700: Color = SemanticHex.red700.color()
     /// 0x991B1B
-    lazy var red800: Color = SemanticHex.red800.color()
+    let red800: Color = SemanticHex.red800.color()
     /// 0x7F1D1D
-    lazy var red900: Color = SemanticHex.red900.color()
+    let red900: Color = SemanticHex.red900.color()
 
     /// 0xFFF7ED
-    lazy var orange50: Color = SemanticHex.orange50.color()
+    let orange50: Color = SemanticHex.orange50.color()
     /// 0xFFEDD5
-    lazy var orange100: Color = SemanticHex.orange100.color()
+    let orange100: Color = SemanticHex.orange100.color()
     /// 0xFED7A7
-    lazy var orange200: Color = SemanticHex.orange200.color()
+    let orange200: Color = SemanticHex.orange200.color()
     /// 0xFDBA74
-    lazy var orange300: Color = SemanticHex.orange300.color()
+    let orange300: Color = SemanticHex.orange300.color()
     /// 0xFB923C
-    lazy var orange400: Color = SemanticHex.orange400.color()
+    let orange400: Color = SemanticHex.orange400.color()
     /// 0xF97316
-    lazy var orange500: Color = SemanticHex.orange500.color()
+    let orange500: Color = SemanticHex.orange500.color()
     /// 0xEA580C
-    lazy var orange600: Color = SemanticHex.orange600.color()
+    let orange600: Color = SemanticHex.orange600.color()
     /// 0xC2410C
-    lazy var orange700: Color = SemanticHex.orange700.color()
+    let orange700: Color = SemanticHex.orange700.color()
     /// 0x9A3412
-    lazy var orange800: Color = SemanticHex.orange800.color()
+    let orange800: Color = SemanticHex.orange800.color()
     /// 0x7C2D12
-    lazy var orange900: Color = SemanticHex.orange900.color()
+    let orange900: Color = SemanticHex.orange900.color()
 
     /// 0xFFFBEA
-    lazy var amber50: Color = SemanticHex.amber50.color()
+    let amber50: Color = SemanticHex.amber50.color()
     /// 0xFEF3C7
-    lazy var amber100: Color = SemanticHex.amber100.color()
+    let amber100: Color = SemanticHex.amber100.color()
     /// 0xFDE68A
-    lazy var amber200: Color = SemanticHex.amber200.color()
+    let amber200: Color = SemanticHex.amber200.color()
     /// 0xFCD34D
-    lazy var amber300: Color = SemanticHex.amber300.color()
+    let amber300: Color = SemanticHex.amber300.color()
     /// 0xFBFB24
-    lazy var amber400: Color = SemanticHex.amber400.color()
+    let amber400: Color = SemanticHex.amber400.color()
     /// 0xF59E0B
-    lazy var amber500: Color = SemanticHex.amber500.color()
+    let amber500: Color = SemanticHex.amber500.color()
     /// 0xD97706
-    lazy var amber600: Color = SemanticHex.amber600.color()
+    let amber600: Color = SemanticHex.amber600.color()
     /// 0xB45309
-    lazy var amber700: Color = SemanticHex.amber700.color()
+    let amber700: Color = SemanticHex.amber700.color()
     /// 0x92400E
-    lazy var amber800: Color = SemanticHex.amber800.color()
+    let amber800: Color = SemanticHex.amber800.color()
     /// 0x78350F
-    lazy var amber900: Color = SemanticHex.amber900.color()
+    let amber900: Color = SemanticHex.amber900.color()
 
     /// 0xF0FDF4
-    lazy var green50: Color = SemanticHex.green50.color()
+    let green50: Color = SemanticHex.green50.color()
     /// 0xDCFCE7
-    lazy var green100: Color = SemanticHex.green100.color()
+    let green100: Color = SemanticHex.green100.color()
     /// 0xBBF7D0
-    lazy var green200: Color = SemanticHex.green200.color()
+    let green200: Color = SemanticHex.green200.color()
     /// 0x86EFAC
-    lazy var green300: Color = SemanticHex.green300.color()
+    let green300: Color = SemanticHex.green300.color()
     /// 0x4ADE80
-    lazy var green400: Color = SemanticHex.green400.color()
+    let green400: Color = SemanticHex.green400.color()
     /// 0x22C55E
-    lazy var green500: Color = SemanticHex.green500.color()
+    let green500: Color = SemanticHex.green500.color()
     /// 0x16A34A
-    lazy var green600: Color = SemanticHex.green600.color()
+    let green600: Color = SemanticHex.green600.color()
     /// 0x15803D
-    lazy var green700: Color = SemanticHex.green700.color()
+    let green700: Color = SemanticHex.green700.color()
     /// 0x166534
-    lazy var green800: Color = SemanticHex.green800.color()
+    let green800: Color = SemanticHex.green800.color()
     /// 0x14532D
-    lazy var green900: Color = SemanticHex.green900.color()
+    let green900: Color = SemanticHex.green900.color()
 
     /// 0xEFF6FF
-    lazy var blue50: Color = SemanticHex.blue50.color()
+    let blue50: Color = SemanticHex.blue50.color()
     /// 0xDBEAFE
-    lazy var blue100: Color = SemanticHex.blue100.color()
+    let blue100: Color = SemanticHex.blue100.color()
     /// 0xBFDBFE
-    lazy var blue200: Color = SemanticHex.blue200.color()
+    let blue200: Color = SemanticHex.blue200.color()
     /// 0x93C5FD
-    lazy var blue300: Color = SemanticHex.blue300.color()
+    let blue300: Color = SemanticHex.blue300.color()
     /// 0x60A5FA
-    lazy var blue400: Color = SemanticHex.blue400.color()
+    let blue400: Color = SemanticHex.blue400.color()
     /// 0x3B82F6
-    lazy var blue500: Color = SemanticHex.blue500.color()
+    let blue500: Color = SemanticHex.blue500.color()
     /// 0x2563EB
-    lazy var blue600: Color = SemanticHex.blue600.color()
+    let blue600: Color = SemanticHex.blue600.color()
     /// 0x1D4ED8
-    lazy var blue700: Color = SemanticHex.blue700.color()
+    let blue700: Color = SemanticHex.blue700.color()
     /// 0x1E40AF
-    lazy var blue800: Color = SemanticHex.blue800.color()
+    let blue800: Color = SemanticHex.blue800.color()
     /// 0x1E3A8A
-    lazy var blue900: Color = SemanticHex.blue900.color()
+    let blue900: Color = SemanticHex.blue900.color()
 }
+
 
 protocol WhereColorProtocol: RawRepresentable where RawValue == UInt {
     var hex: RawValue { get }
