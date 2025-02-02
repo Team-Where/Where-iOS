62bae59ba90b3d41931f0aae089c6ea409e6e8f7
2HYUNHO <soda9503@gmail.com>
2025-02-02 21:36:17 +0900

    [Feat] 코멘트 남기기 버튼 생성

--- Where/Views/Place/CommentView.swift
+++ Where/Views/Place/CommentView.swift
@@ -8,61 +8,61 @@
 import SwiftUI
 
 struct CommentView: View {
+    @State private var comments: [String] = []
+    @State private var commentText: String = ""
+    @State private var isShowingCommentSheet: Bool = false
+    
     var body: some View {
         VStack {
             HStack(spacing: 4) {
                 Text("코멘트")
                 
-                Text("2")
+                Text("\(comments.count)")
                 
                 Spacer()
             }
             .whereFont(.body16medium)
             
+            Button {
+                isShowingCommentSheet = true
+            } label: {
+                Text("코멘트 남기기")
+                    .foregroundStyle(Color(hex: 0x4F46E5))
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 48)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(Color(hex: 0xDEE2E6), lineWidth: 1)
+                    )
+            }
+            
             VStack {
-                HStack {
-                    Text("여기 웨이팅 맛집임")
-                        .whereFont(.body14regular)
-                        .foregroundStyle(.black)
-                        .padding(.vertical, 10)
-                        .padding(.horizontal, 16)
-                        .background(
-                            RoundedRectangle(cornerRadius: 12)
-                                .stroke(Color(hex: 0x4F46E5), lineWidth: 1)
-                        )
-                    
-                    Spacer()
-                }
-                
-                HStack {
-                    Text("여기 야경 맛집임")
-                        .whereFont(.body14regular)
-                        .foregroundStyle(.black)
-                        .padding(.vertical, 10)
-                        .padding(.horizontal, 16)
-                        .background(
-                            RoundedRectangle(cornerRadius: 12)
-                                .stroke(Color(hex: 0xE3E4E9), lineWidth: 1)
-                        )
-                    
-                    Spacer()
-                }
-                
-                HStack {
-                    Text("그냥 그럼")
-                        .whereFont(.body14regular)
-                        .foregroundStyle(.black)
-                        .padding(.vertical, 10)
-                        .padding(.horizontal, 16)
-                        .background(
-                            RoundedRectangle(cornerRadius: 12)
-                                .stroke(Color(hex: 0xE3E4E9), lineWidth: 1)
-                        )
-                    
-                    Spacer()
+                ForEach(comments, id: \.self) { comment in
+                    HStack {
+                        Text(comment)
+                            .whereFont(.body14regular)
+                            .foregroundStyle(.black)
+                            .padding(.vertical, 10)
+                            .padding(.horizontal, 16)
+                            .background(
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(Color(hex: 0x4F46E5), lineWidth: 1)
+                            )
+                        
+                        Spacer()
+                    }
                 }
             }
             .padding(.top, 16)
+            
+            HStack {
+                TextField("코멘트 남기기", text: $commentText) {
+                    if !commentText.isEmpty {
+                        comments.append(commentText)
+                        commentText = ""
+                    }
+                }
+            }
             
             Spacer()
         }
