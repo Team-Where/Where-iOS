974e76b5d4979a0b425b234d886bae7c297b80d8
YunSwain <destap@naver.com>
2025-03-02 19:07:57 +0900

    [Feat]: 기존 BasedFormView 스타일 수정자 추가

---
+++ Where/Utility/DesignSystem/Components/WhereFormModifier.swift
@@ -0,0 +1,48 @@
+//
+//  WhereFormModifier.swift
+//  Where
+//
+//  Created by Swain Yun on 3/2/25.
+//
+
+import SwiftUI
+
+struct WhereFormModifier<ActionButton: View>: ViewModifier {
+    let navigationTitle: String
+    let actionButton: ActionButton
+    
+    init(_ navigationTitle: String, actionButton: ActionButton) {
+        self.navigationTitle = navigationTitle
+        self.actionButton = actionButton
+    }
+    
+    func body(content: Content) -> some View {
+        VStack {
+            Text(navigationTitle)
+                .whereFont(.title24semibold)
+                .foregroundStyle(.where(.gray800))
+                .multilineTextAlignment(.leading)
+                .padding(.top)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            content
+            
+            Spacer()
+            
+            actionButton
+        }
+        .padding()
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                BackButton()
+            }
+        }
+    }
+}
+
+extension View {
+    func whereForm<ActionButton: View>(_ title: String, @ViewBuilder actionButton: @escaping () -> ActionButton) -> some View {
+        modifier(WhereFormModifier(title, actionButton: actionButton()))
+    }
+}
