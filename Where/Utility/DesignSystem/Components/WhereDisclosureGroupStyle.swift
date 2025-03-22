a21c7fa233019b193d6c795a861e2a6241026eec
YunSwain <destap@naver.com>
2025-03-22 19:52:51 +0900

    [Feat]: FAQ, 공지사항 화면에서 쓸 DisclosureGroupStyle 정의

---
+++ Where/Utility/DesignSystem/Components/WhereDisclosureGroupStyle.swift
@@ -0,0 +1,51 @@
+//
+//  WhereDisclosureGroupStyle.swift
+//  Where
+//
+//  Created by Swain Yun on 3/22/25.
+//
+
+import SwiftUI
+
+struct WhereDisclosureGroupStyle: DisclosureGroupStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        VStack {
+            Button {
+                withAnimation {
+                    configuration.isExpanded.toggle()
+                }
+            } label: {
+                HStack {
+                    Text("Q")
+                        .foregroundStyle(.accent)
+                    
+                    configuration.label
+                        .foregroundStyle(.where(.gray800))
+                    
+                    Spacer()
+                    
+                    Image(systemName: "chevron.down")
+                        .resizable()
+                        .rotationEffect(configuration.isExpanded ? .degrees(-180) : .degrees(0))
+                        .animation(.easeInOut(duration: 0.2), value: configuration.isExpanded)
+                        .foregroundStyle(.where(.gray500))
+                        .frame(width: 8, height: 4)
+                }
+                .frame(height: 60)
+            }
+            .buttonStyle(.plain)
+            
+            if configuration.isExpanded {
+                configuration.content
+                    .whereFont(.body14regular)
+                    .foregroundStyle(.where(.gray700))
+                    .padding(16)
+                    .background(
+                        RoundedRectangle(cornerRadius: 16)
+                            .foregroundStyle(.where(.gray100))
+                    )
+            }
+        }
+        .padding(.horizontal)
+    }
+}
