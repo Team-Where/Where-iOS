d8281033d7525fea376bb8cbeb476304714e66ad
YunSwain <destap@naver.com>
2025-03-24 18:30:33 +0900

    [Feat]: 1:1 문의 화면 추가

---
+++ Where/Views/SideMenu/InquiryView.swift
@@ -0,0 +1,245 @@
+//
+//  InquiryView.swift
+//  Where
+//
+//  Created by Swain Yun on 3/24/25.
+//
+
+import SwiftUI
+
+struct InquiryView: View {
+    @State private var inquiries: [Inquiry] = [
+        .init(id: 0, createdAt: .now, modifiedAt: .now, title: "[비밀번호 변경이 안돼요]", content: "비밀번호가 변경이 안돼요. 해결해주세요.", images: [], isAnswered: false, answerContent: nil),
+        .init(id: 1, createdAt: .now, modifiedAt: .now, title: "[친구가 추가되지 않아요]", content: "친구가 추가되지 않아요. 사진에 보이는 것처럼 등록했는데, 어떻게 해야하는 걸까요?", images: [], isAnswered: true, answerContent: "안녕하세요. 어디서비스 운영자입니다.\n서비스 상에 오류가 있어 불편함드려 죄송합니다. 오류는 금일 밤 12시에 수정되어 정상적으로 서비스를 이용하실 수 있습니다. 감사합니다.")
+    ]
+    
+    private var waitingForReplyInquiries: [Inquiry] {
+        inquiries.filter { $0.isAnswered == false }
+    }
+    
+    private var answerCompleteInquiries: [Inquiry] {
+        inquiries.filter { $0.isAnswered == true }
+    }
+    
+    var body: some View {
+        SelectionTab<TabViewItem>(selection: [
+            .waitingForReply(inquiries: waitingForReplyInquiries),
+            .answerComplete(inquiries: answerCompleteInquiries)
+        ])
+        .navigationBarBackButtonHidden()
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                BackButton()
+            }
+            
+            ToolbarItem(placement: .principal) {
+                Text("FAQ")
+                    .whereFont(.subtitle18semibold)
+                    .foregroundStyle(.where(.gray800))
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "pencil.line")
+                        .foregroundStyle(.where(.gray800))
+                }
+            }
+        }
+    }
+}
+
+// MARK: Nested Types
+extension InquiryView {
+    enum TabViewItem: SelectionTabItem {
+        case waitingForReply(inquiries: [Inquiry])
+        case answerComplete(inquiries: [Inquiry])
+        
+        var id: String { String(describing: self) }
+        
+        var title: String {
+            switch self {
+            case .waitingForReply(let inquiries): return "답변대기(\(inquiries.count))"
+            case .answerComplete(let inquiries): return "답변완료(\(inquiries.count))"
+            }
+        }
+        
+        @ViewBuilder func view() -> some View {
+            switch self {
+            case .waitingForReply(let inquiries): InquiryListView(inquiries: inquiries)
+            case .answerComplete(let inquiries): InquiryListView(inquiries: inquiries)
+            }
+        }
+    }
+    
+    struct InquiryListView: View {
+        @State private var sheetType: SheetType?
+        
+        let inquiries: [Inquiry]
+        
+        var body: some View {
+            if inquiries.isEmpty {
+                unavailableView
+            } else {
+                inquiriesList
+            }
+        }
+        
+        private var unavailableView: some View {
+            VStack(spacing: 10) {
+                Spacer()
+                
+                Image(systemName: "exclamationmark.circle")
+                    .foregroundStyle(.where(.gray600))
+                
+                Text("아직 1:1 문의 사항이 없어요!")
+                    .whereFont(.body16medium)
+                    .foregroundStyle(.where(.gray700))
+                
+                Spacer()
+            }
+        }
+        
+        private var inquiriesList: some View {
+            ScrollView(.vertical) {
+                LazyVStack {
+                    ForEach(inquiries) { inquiry in
+                        cell(inquiry)
+                            .listRowSeparator(.hidden)
+                    }
+                }
+            }
+            .sheet(item: $sheetType) { type in
+                switch type {
+                case .deleteInquiry(let inquiry):
+                    DeleteInquirySheet(inquiry: inquiry)
+                }
+            }
+        }
+        
+        @ViewBuilder private func cell(_ inquiry: Inquiry) -> some View {
+            DisclosureGroup {
+                VStack(alignment: .leading, spacing: 40) {
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("문의")
+                            .whereFont(.caption12regular)
+                            .foregroundStyle(.where(.gray800))
+                            .padding(.vertical, 4)
+                            .padding(.horizontal, 8)
+                            .background(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(.white)
+                            )
+                        
+                        Text(inquiry.content)
+                            .whereFont(.body14regular)
+                            .foregroundStyle(.where(.gray700))
+                            .multilineTextAlignment(.leading)
+                    }
+                    
+                    HStack(spacing: 10) {
+                        Spacer()
+                        
+                        Button {
+                            // TODO: 문의 수정 기능 연결
+                        } label: {
+                            Text("수정")
+                                .whereFont(.body14medium)
+                                .foregroundStyle(.accent)
+                                .frame(width: 52, height: 32)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .fill(.white)
+                                        .strokeBorder(.accent)
+                                )
+                        }
+                        
+                        Button {
+                            sheetType = .deleteInquiry(inquiry: inquiry)
+                        } label: {
+                            Text("삭제")
+                                .whereFont(.body14medium)
+                                .foregroundStyle(.accent)
+                                .frame(width: 52, height: 32)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .fill(.white)
+                                        .strokeBorder(.accent)
+                                )
+                        }
+                    }
+                    
+                    if let answerContent = inquiry.answerContent {
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("답변")
+                                .whereFont(.caption12regular)
+                                .foregroundStyle(.where(.gray800))
+                                .padding(.vertical, 4)
+                                .padding(.horizontal, 8)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .fill(.white)
+                                )
+                            
+                            Text(answerContent)
+                                .whereFont(.body14regular)
+                                .foregroundStyle(.where(.gray700))
+                        }
+                    }
+                }
+            } label: {
+                HStack {
+                    VStack(alignment: .leading, spacing: 6) {
+                        Text(inquiry.title)
+                            .whereFont(.body16medium)
+                            .foregroundStyle(.where(.gray800))
+                        
+                        AsyncDateView(date: .constant(inquiry.createdAt), format: .yyyyMMdd, prompt: "")
+                            .whereFont(.body14regular)
+                            .foregroundStyle(.where(.gray700))
+                    }
+                    
+                    Spacer()
+                }
+            }
+            .disclosureGroupStyle(WhereDisclosureGroupStyle(labelHeight: 74))
+        }
+    }
+}
+
+// MARK: Sheet
+extension InquiryView {
+    /// 문의 화면에서 라우팅 가능한 시트의 종류
+    enum SheetType: Identifiable {
+        case deleteInquiry(inquiry: Inquiry)
+        
+        var id: String { String(describing: self) }
+    }
+    
+    struct DeleteInquirySheet: View {
+        let inquiry: Inquiry
+        
+        var body: some View {
+            Button {
+                // TODO: 문의 삭제 기능 연결
+            } label: {
+                Text("1:1 문의 삭제")
+                    .whereFont(.body16medium)
+                    .frame(height: 54)
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.whereRoundedProminent(foreground: .red, background: .where(.gray100)))
+            .presentationDetents([.fraction(0.17)])
+            .presentationCornerRadius(16)
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        InquiryView()
+    }
+}
