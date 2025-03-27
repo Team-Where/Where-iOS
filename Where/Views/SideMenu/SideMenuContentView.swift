fe5d253d604d98fbd05c60b19ad7b7064129c9ec
YunSwain <destap@naver.com>
2025-03-27 19:57:32 +0900

    [Refactor]: 사이드메뉴 화면 디자인 가이드 맞추어 수정

--- Where/Views/SideMenu/SideMenuContentView.swift
+++ Where/Views/SideMenu/SideMenuContentView.swift
@@ -8,40 +8,11 @@
 import SwiftUI
 
 struct SideMenuContentView: View {
-    @Binding var isSideMenu: Bool // 사이드 메뉴 상태
-    
-    // 기기별 Safe Area 상단 여백을 계산
-    private func safeAreaTopPadding() -> CGFloat {
-        guard let window = UIApplication.shared.windows.first else { return 0 }
-        return window.safeAreaInsets.top // 안전 영역 + 추가 여백
-    }
+    @Binding var isSideMenuPresented: Bool // 사이드 메뉴 상태
+    @State private var navigationType: NavigationType?
     
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                Button {
-                    withAnimation(.easeInOut(duration: 0.3)) {
-                        isSideMenu = false
-                    }
-                } label: {
-                    Image(systemName: "xmark")
-                        .foregroundColor(.black)
-                        .padding()
-                }
-
-                Spacer()
-
-                Button {
-                    // 설정 버튼 액션
-                } label: {
-                    Text("설정")
-                        .whereFont(.body16medium)
-                        .foregroundStyle(Color(hex: 0x4F46E5))
-                        .padding(.trailing, 16)
-                }
-            }
-            .padding(.top, safeAreaTopPadding())
-            
+        VStack(alignment: .leading, spacing: .zero) {
             // 프로필 섹션
             HStack {
                 VStack(alignment: .leading) {
@@ -69,7 +40,7 @@ struct SideMenuContentView: View {
                     .resizable()
                     .frame(width: 80, height: 80)
             }
-            .padding(.top, 16)
+            .padding(.top, 40)
             .padding(.horizontal)
 
             // 구분선
@@ -83,38 +54,95 @@ struct SideMenuContentView: View {
             }
             
             // 메뉴 리스트
-            VStack(alignment: .leading, spacing: 0) {
-                SideMenuItem(icon: "bell", title: "알림")
-                SideMenuItem(icon: "bubble", title: "FAQ")
-                SideMenuItem(icon: "square.and.pencil", title: "1:1 문의")
-                SideMenuItem(icon: "megaphone", title: "공지사항")
+            VStack(alignment: .leading, spacing: 32) {
+                SideMenuItem($navigationType, type: .settings, title: "설정", iconName: "gear")
+                SideMenuItem($navigationType, type: .notifications, title: "알림", iconName: "bell")
+                SideMenuItem($navigationType, type: .FAQs, title: "FAQ", iconName: "bubble")
+                SideMenuItem($navigationType, type: .inquiries, title: "1:1 문의", iconName: "square.and.pencil")
+                SideMenuItem($navigationType, type: .announcements, title: "공지사항", iconName: "megaphone")
             }
             .padding(.horizontal)
 
             Spacer()
         }
         .frame(maxWidth: .infinity, alignment: .leading)
-        .background(Color.white)
+        .navigationDestination(item: $navigationType) { type in
+            switch type {
+            case .settings: SettingView()
+            case .notifications: NotificationListView()
+            case .FAQs: FAQView()
+            case .inquiries: InquiryView()
+            case .announcements: AnnouncementView()
+            case .editProfile: EmptyView()
+            }
+        }
     }
 }
 
-// 사이드 메뉴 아이템
-struct SideMenuItem: View {
-    var icon: String
-    var title: String
-
-    var body: some View {
-        HStack(spacing: 12) {
-            Image(systemName: icon)
-                .foregroundStyle(.black)
-                .frame(width: 18, height: 18)
-            
-            Text(title)
-                .whereFont(.body16regular)
-                .foregroundStyle(.black)
-            
-            Spacer()
+// MARK: Nested Types
+extension SideMenuContentView {
+    // 사이드 메뉴 아이템
+    struct SideMenuItem: View {
+        @Binding var selectedNavigationType: NavigationType?
+        let type: NavigationType
+        let title: String
+        let iconName: String
+        
+        init(
+            _ selected: Binding<NavigationType?>,
+            type: NavigationType,
+            title: String,
+            iconName: String
+        ) {
+            self._selectedNavigationType = selected
+            self.type = type
+            self.title = title
+            self.iconName = iconName
         }
-        .padding(.bottom, 32)
+
+        var body: some View {
+            Button {
+                selectedNavigationType = type
+            } label: {
+                HStack(spacing: 12) {
+                    Image(systemName: iconName)
+                        .foregroundStyle(.black)
+                        .frame(width: 18, height: 18)
+                    
+                    Text(title)
+                        .whereFont(.body16regular)
+                        .foregroundStyle(.black)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "chevron.right")
+                        .resizable()
+                        .frame(width: 4, height: 8)
+                        .foregroundStyle(.where(.gray600))
+                }
+                .whereFont(.subtitle18semibold)
+                .foregroundStyle(.where(.gray900))
+            }
+        }
     }
+    
+    /// SideMenuContentView 화면에서 라우팅할 수 있는 NavigationPath의 종류
+    enum NavigationType {
+        /// 설정
+        case settings
+        /// 알림 목록
+        case notifications
+        /// FAQ
+        case FAQs
+        /// 1:1 문의
+        case inquiries
+        /// 공지사항
+        case announcements
+        /// 프로필 수정
+        case editProfile
+    }
+}
+
+#Preview {
+    TabBarView()
 }
