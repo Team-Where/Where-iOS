dca68ebdf0b50d7aeb121c7b2b01eafc0c9253a7
YunSwain <destap@naver.com>
2025-03-27 19:56:35 +0900

    [Refactor]: 사이드메뉴와 연관없는 화면의 사이드메뉴 코드 정리

--- Where/Utility/DesignSystem/Components/TabBarView.swift
+++ Where/Utility/DesignSystem/Components/TabBarView.swift
@@ -11,77 +11,54 @@ struct TabBarView: View {
     @State private var selectedTab = 0 // 현재 선택된 탭의 인덱스
     @State private var showNewMeeting = false // sheet 표시 여부
     @State private var previousTab = 0 // 이전 탭 저장
-    @State private var isSideMenu: Bool = false
     
     var body: some View {
-        ZStack {
-            TabView(selection: $selectedTab) {
-                // 내모임 뷰
-                HomeView(isSideMenu: $isSideMenu)
-                    .tabItem {
-                        Label("내모임", systemImage: "person.2")
-                            .environment(\.symbolVariants, .none)
+        TabView(selection: $selectedTab) {
+            // 내모임 뷰
+            NavigationStack {
+                HomeView()
+                    .overlay(alignment: .bottom) {
+                        Divider()
                     }
-                    .tag(0)
-                
-                // 새 모임 만들기
-                Color.clear
-                    .tabItem {
-                        Image("BottomPlus")
-                    }
-                    .tag(1)
-                
-                // 친구목록 뷰
+            }
+            .tabItem {
+                Label("내모임", systemImage: "person.2")
+                    .environment(\.symbolVariants, .none)
+            }
+            .tag(0)
+            
+            // 새 모임 만들기
+            Color.clear
+                .tabItem {
+                    Image("BottomPlus")
+                }
+                .tag(1)
+            
+            // 친구목록 뷰
+            NavigationStack {
                 FriendsListView()
-                    .tabItem {
-                        Label("친구목록", systemImage: "list.bullet")
+                    .overlay(alignment: .bottom) {
+                        Divider()
                     }
-                    .tag(2)
             }
-            .tint(.black) // 선택된 탭 아이템의 색상
-            .onChange(of: selectedTab) { oldValue, newValue in
-                if newValue != 1 {
-                    previousTab = newValue
-                } else {
-                    showNewMeeting = true
-                    selectedTab = previousTab
-                }
+            .tabItem {
+                Label("친구목록", systemImage: "list.bullet")
             }
-            .sheet(isPresented: $showNewMeeting) {
-                NewMeetingSheet1View(showNewMeeting: $showNewMeeting)
-                    .presentationCornerRadius(24)
-            }
-            VStack {
-                Spacer()
-                Rectangle()
-                    .frame(maxWidth: .infinity, maxHeight: 1)
-                    .foregroundColor(Color(hex: 0xE5E7EB))
-                    .padding(.bottom, 60)
-            }
-            // 사이드메뉴 추가
-            ZStack {
-                // 배경 탭하면 사이드메뉴 닫힘
-                Color.black.opacity(isSideMenu ? 0.3 : 0)
-                    .ignoresSafeArea()
-                    .onTapGesture {
-                        withAnimation(.easeInOut(duration: 0.3)) {
-                            isSideMenu = false
-                        }
-                    }
-                
-                // 사이드메뉴 뷰
-                SideMenuView($isSideMenu, width: 370) {
-                    VStack {
-                        SideMenuContentView(isSideMenu: $isSideMenu)
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                }
-                .offset(x: isSideMenu ? 0 : 370) // 오른쪽에서 왼쪽으로 생성
-                .animation(.easeInOut(duration: 0.3), value: isSideMenu)
-            }
-            .ignoresSafeArea(edges: .all)
+            .tag(2)
         }
-        .navigationBarBackButtonHidden()
+        .tint(.black) // 선택된 탭 아이템의 색상
+        .onChange(of: selectedTab) { oldValue, newValue in
+            if newValue != 1 {
+                previousTab = newValue
+            } else {
+                showNewMeeting = true
+                selectedTab = previousTab
+            }
+        }
+        .sheet(isPresented: $showNewMeeting) {
+            NewMeetingSheet1View(showNewMeeting: $showNewMeeting)
+                .presentationCornerRadius(24)
+        }
     }
 }
 
