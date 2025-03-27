b7b3db8d79155c57e56972408053fe9ca1598d7d
YunSwain <destap@naver.com>
2025-03-27 19:56:08 +0900

    [Feat]: 기존 SideMenuView를 수정자로 변경해 SideMenu 기능 추가

---
+++ Where/Utility/DesignSystem/Components/SideMenu.swift
@@ -0,0 +1,73 @@
+//
+//  SideMenu.swift
+//  Where
+//
+//  Created by Swain Yun on 3/27/25.
+//
+
+import SwiftUI
+
+struct SideMenuModifier<SideMenuContent: View>: ViewModifier {
+    @Binding var isPresented: Bool
+    @ViewBuilder let sideMenuContent: () -> SideMenuContent
+    let sideMenuWidthRatio: CGFloat = 0.95
+    
+    @State private var rootViewSize: CGSize = .zero
+    @State private var sideMenuWidth: CGFloat = .zero
+    
+    init(
+        _ isPresented: Binding<Bool>,
+        @ViewBuilder content: @escaping () -> SideMenuContent
+    ) {
+        self._isPresented = isPresented
+        self.sideMenuContent = content
+    }
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .trailing) {
+            content
+                .onGeometryChange(for: CGSize.self) { proxy in
+                    proxy.size
+                } action: { newValue in
+                    rootViewSize = newValue
+                    sideMenuWidth = newValue.width * sideMenuWidthRatio
+                }
+                .overlay(
+                    isPresented ?
+                    Color.black.opacity(0.3)
+                        .onTapGesture {
+                            withAnimation {
+                                isPresented = false
+                            }
+                        }
+                    : nil
+                )
+            
+            if isPresented {
+                sideMenuView()
+            }
+        }
+        .animation(.easeInOut, value: isPresented)
+    }
+    
+    @ViewBuilder private func sideMenuView() -> some View {
+        sideMenuContent()
+            .frame(width: sideMenuWidth)
+            .background(.white)
+            .transition(.move(edge: .trailing))
+            .zIndex(1)
+    }
+}
+
+extension View {
+    func sideMenu<SideMenuContent: View>(
+        isPresented: Binding<Bool>,
+        @ViewBuilder content: @escaping () -> SideMenuContent
+    ) -> some View {
+        modifier(SideMenuModifier<SideMenuContent>(isPresented, content: content))
+    }
+}
+
+#Preview {
+    TabBarView()
+}
