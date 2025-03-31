029333d43e2d7b9eb95e20531fa74d4e0c78e35c
YunSwain <destap@naver.com>
2025-03-31 21:44:31 +0900

    [Feat]: Tokens 구조체 선언

--- Where/Service/Storage/TokenStorage.swift
+++ Where/Service/Storage/TokenStorage.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Security
 
-protocol TokenStorageProtocol {
+protocol TokenStorageProtocol: Sendable {
     typealias Query = [String: Any]
     typealias Key = UInt64
     
---
+++ Where/Service/Storage/Tokens.swift
@@ -0,0 +1,13 @@
+//
+//  Tokens.swift
+//  Where
+//
+//  Created by Swain Yun on 3/31/25.
+//
+
+import Foundation
+
+struct Tokens: Codable {
+    let accessToken: String
+    let refreshToken: String
+}
