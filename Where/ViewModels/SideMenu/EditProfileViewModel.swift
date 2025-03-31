a69f56b30f736afcc9c63baa51710f88f57430de
YunSwain <destap@naver.com>
2025-03-31 11:46:20 +0900

    [Refactor]: EditProfileViewModel 초기화 로직 수정 및 코드 정리

--- Where/ViewModels/SideMenu/EditProfileViewModel.swift
+++ Where/ViewModels/SideMenu/EditProfileViewModel.swift
@@ -11,28 +11,34 @@ import Combine
 final class EditProfileViewModel: ObservableObject {
     @Published var isPopupPresented: Bool = false
     @Published var profileImage: UIImage?
-    @Published var nicknameFieldText: String
+    @Published var nicknameFieldText: String = String()
     @Published var isNicknameValid: Bool = false
     
+    private let auth: any AuthentificationCoreProtocol
     private var cancellables = Set<AnyCancellable>()
     
-    init(user: User) {
-        defer { subscribe() }
-        
-        guard let imageURL = user.imageURL,
-              let data = try? Data(contentsOf: imageURL),
-              let image = UIImage(data: data)
-        else {
-            self.profileImage = UIImage(named: "person")
-            self.nicknameFieldText = user.nickname
-            return
-        }
-        
-        self.profileImage = image
-        self.nicknameFieldText = user.nickname
+    init(auth: any AuthentificationCoreProtocol) {
+        self.auth = auth
+        subscribe()
     }
     
     private func subscribe() {
+        auth.user
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+#if DEBUG
+                    print(error)
+#endif
+                }
+            } receiveValue: { [weak self] user in
+                self?.initializeProperties(user)
+            }
+            .store(in: &cancellables)
+        
         $nicknameFieldText
             .removeDuplicates()
             .receive(on: DispatchQueue.main)
@@ -40,6 +46,20 @@ final class EditProfileViewModel: ObservableObject {
                 self?.isNicknameValid = nickname.isValidNickname()
             }
             .store(in: &cancellables)
+    }
+    
+    private func initializeProperties(_ user: User?) {
+        guard let imageURL = user?.imageURL,
+              let data = try? Data(contentsOf: imageURL),
+              let image = UIImage(data: data)
+        else {
+            self.profileImage = UIImage(named: "person")
+            self.nicknameFieldText = user?.nickname ?? ""
+            return
+        }
+        
+        self.profileImage = image
+        self.nicknameFieldText = user?.nickname ?? ""
     }
 }
 
