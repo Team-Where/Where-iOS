bf49e00ea7d1808d6762f6796750dd1e20b8b62c
YunSwain <destap@naver.com>
2025-03-31 23:14:22 +0900

    [Move]: Nested Type 도메인 모델로 추출

--- Where/Views/Meeting/CreateMeetingSheet.swift
+++ Where/Views/Meeting/CreateMeetingSheet.swift
@@ -99,32 +99,6 @@ extension CreateMeetingSheet {
             }
         }
     }
-    
-    /// 임시 모임 정보
-    struct TemporaryMeetingInfo {
-        /// 모임 제목
-        let title: String
-        /// 모임 설명
-        let description: String
-        /// 모임을 생성한 사용자의 식별자
-        let creatorId: UInt64
-        /// 초대받는 인원들의 식별자
-        let participants: [UInt64]
-        /// 모임 대표 이미지
-        let image: UIImage?
-        
-        static func initialize(_ userId: UInt64) -> Self {
-            TemporaryMeetingInfo(title: String(), description: String(), creatorId: userId, participants: [], image: nil)
-        }
-        
-        func setBasicInfo(title: String, description: String, image: UIImage?) -> Self {
-            TemporaryMeetingInfo(title: title, description: description, creatorId: self.creatorId, participants: self.participants, image: image)
-        }
-        
-        func setInvitedFriends(_ participants: [UInt64]) -> Self {
-            TemporaryMeetingInfo(title: self.title, description: self.description, creatorId: self.creatorId, participants: participants, image: self.image)
-        }
-    }
 }
 
 // MARK: Subviews
