53e8c20cfd22b5ac5d1ea785dd951a62f3833f41
YunSwain <destap@naver.com>
2025-03-31 23:14:44 +0900

    [Feat]: CommunityCoreProtocol 정의 (WIP)

---
+++ Where/Service/Core/CommunityCore.swift
@@ -0,0 +1,80 @@
+//
+//  CommunityCore.swift
+//  Where
+//
+//  Created by Swain Yun on 3/30/25.
+//
+
+import SwiftUI
+import Combine
+
+protocol CommunityCoreProtocol: ObservableObject {
+    /// 나의 친구 목록
+    var friends: AnyPublisher<[UInt64: User], CommunityCoreError> { get }
+    /// 나와 연관된 모임 목록
+    var meetings: AnyPublisher<[UInt64: Meeting], CommunityCoreError> { get }
+    
+    /// 친구 추가
+    func createFriend(friend: User)
+    /// 친구 목록 조회
+    func readFriends()
+    /// 친구 삭제
+    /// - Parameters:
+    ///     - id: 삭제할 대상의 고유 식별자
+    func deleteFriend(id: UInt64)
+    /// 친구 즐겨찾기 토글
+    /// - Parameters:
+    ///     - id: 즐겨찾기 설정 또는 해제할 대상의 고유 식별자
+    func toggleBookmarkFriend(id: UInt64)
+    /// 모임 일정 등록
+    /// - Parameters:
+    ///     - id: 모임의 고유 식별자
+    func createSchedule(id: UInt64)
+    /// 모임 일정 조회
+    /// - Parameters:
+    ///     - id: 모임의 고유 식별자
+    ///
+    func readSchedule(id: UInt64)
+    /// 모임 일정 수정
+    /// - Parameters:
+    ///     - id: 모임의 고유 식별자
+    func updateSchedule(id: UInt64)
+    /// 모임 일정 삭제
+    /// - Parameters:
+    ///     - id: 모임의 고유 식별자
+    func deleteSchedule(id: UInt64)
+    /// 모임 생성
+    /// - Parameters:
+    ///     - info: 생성 중인 모임의 임시 정보
+    func createMeeting(info: TemporaryMeetingInfo)
+    /// 모임 조회
+    func readMeetings()
+    /// 모임 수정
+    /// - Parameters:
+    ///     - id: 모임의 고유 식별자
+    ///     - title: 모임 제목
+    ///     - description: 모임 설명
+    ///     - image: 모임 대표 이미지
+    func updateMeeting(id: UInt64, title: String?, description: String?, image: UIImage?)
+    /// 모임 탈퇴
+    /// - Parameters:
+    ///     - id: 모임의 고유 식별자
+    func exitMeeting(id: UInt64)
+    /// 모임 초대현황 조회
+    /// - Parameters:
+    ///     - id: 모임의 고유 식별자
+    func readParticipants(id: UInt64)
+    /// 모임 초대
+    /// - Parameters:
+    ///     - id: 모임의 고유 식별자
+    ///     - participantId: 초대 대상의 식별자
+    func inviteParticipant(id: UInt64, participantId: UInt64)
+    /// 모임 초대 수락
+    /// - Parameters:
+    ///     - id: 초대장 식별자
+    func acceptInvitation(id: UInt64)
+}
+
+enum CommunityCoreError: Error {
+    
+}
