5fccb4449a60c784fa180bb911ddc731798a4b0c
YunSwain <destap@naver.com>
2025-04-01 21:06:02 +0900

    [Feat]: CommunityCoreProtocol 구현체 추가 (WIP)

--- Where/Service/Core/CommunityCore.swift
+++ Where/Service/Core/CommunityCore.swift
@@ -13,6 +13,8 @@ protocol CommunityCoreProtocol: ObservableObject {
     var friends: AnyPublisher<[UInt64: User], CommunityCoreError> { get }
     /// 나와 연관된 모임 목록
     var meetings: AnyPublisher<[UInt64: Meeting], CommunityCoreError> { get }
+    /// 사용자 식별자
+    var userId: UInt64? { get }
     
     /// 친구 추가
     func createFriend(friend: User)
@@ -77,4 +79,127 @@ protocol CommunityCoreProtocol: ObservableObject {
 
 enum CommunityCoreError: Error {
     
+}
+
+final class CommunityCore {
+    @Published var _friends: [UInt64: User] = [:]
+    @Published var _meetings: [UInt64: Meeting] = [:]
+    @Published private(set) var userId: UInt64?
+    
+    private let networkService: NetworkServiceProtocol
+    private let tokenStorage: TokenStorageProtocol
+    private let auth: AuthentificationCoreProtocol
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(
+        networkService: NetworkServiceProtocol,
+        tokenStorage: TokenStorageProtocol,
+        auth: AuthentificationCoreProtocol
+    ) {
+        self.networkService = networkService
+        self.tokenStorage = tokenStorage
+        self.auth = auth
+        subscribe()
+    }
+    
+    private func subscribe() {
+        auth.user
+            .sink { completion in
+                switch completion {
+                case .finished: break
+                case .failure(let error):
+#if DEBUG
+                    print(error)
+#endif
+                }
+            } receiveValue: { [weak self] user in
+                guard let userId = user?.id else {
+                    self?.userId = nil
+                    self?._friends = [:]
+                    self?._meetings = [:]
+                    return
+                }
+                self?.userId = userId
+                self?.readFriends()
+                self?.readMeetings()
+            }
+            .store(in: &cancellables)
+    }
+}
+
+// MARK: CommunityCoreProtocol Confirmation
+extension CommunityCore: CommunityCoreProtocol {
+    var friends: AnyPublisher<[UInt64: User], CommunityCoreError> {
+        $_friends
+            .map { $0 }
+            .setFailureType(to: CommunityCoreError.self)
+            .eraseToAnyPublisher()
+    }
+    
+    var meetings: AnyPublisher<[UInt64: Meeting], CommunityCoreError> {
+        $_meetings
+            .map { $0 }
+            .setFailureType(to: CommunityCoreError.self)
+            .eraseToAnyPublisher()
+    }
+    
+    func createFriend(friend: User) {
+        
+    }
+    
+    func readFriends() {
+        
+    }
+    
+    func deleteFriend(id: UInt64) {
+        
+    }
+    
+    func toggleBookmarkFriend(id: UInt64) {
+        
+    }
+    
+    func createSchedule(id: UInt64) {
+        
+    }
+    
+    func readSchedule(id: UInt64) {
+        
+    }
+    
+    func updateSchedule(id: UInt64) {
+        
+    }
+    
+    func deleteSchedule(id: UInt64) {
+        
+    }
+    
+    func createMeeting(info: TemporaryMeetingInfo) {
+        
+    }
+    
+    func readMeetings() {
+        
+    }
+    
+    func updateMeeting(id: UInt64, title: String?, description: String?, image: UIImage?) {
+        
+    }
+    
+    func exitMeeting(id: UInt64) {
+        
+    }
+    
+    func readParticipants(id: UInt64) {
+        
+    }
+    
+    func inviteParticipant(id: UInt64, participantId: UInt64) {
+        
+    }
+    
+    func acceptInvitation(id: UInt64) {
+        
+    }
 }
