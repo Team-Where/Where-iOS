bfa631c42233e25edcd714fa0f257129d013bca4
YunSwain <destap@naver.com>
2025-04-04 15:17:46 +0900

    [Refactor]: 임시 모델 수정

--- Where/Entities/TemporaryMeetingInfo.swift
+++ Where/Entities/TemporaryMeetingInfo.swift
@@ -13,22 +13,20 @@ struct TemporaryMeetingInfo {
     let title: String
     /// 모임 설명
     let description: String
-    /// 모임을 생성한 사용자의 식별자
-    let creatorId: UInt64
     /// 초대받는 인원들의 식별자
     let participants: [UInt64]
     /// 모임 대표 이미지
     let image: UIImage?
     
-    static func initialize(_ userId: UInt64) -> Self {
-        TemporaryMeetingInfo(title: String(), description: String(), creatorId: userId, participants: [], image: nil)
+    static func initialize() -> Self {
+        TemporaryMeetingInfo(title: String(), description: String(), participants: [], image: nil)
     }
     
     func setBasicInfo(title: String, description: String, image: UIImage?) -> Self {
-        TemporaryMeetingInfo(title: title, description: description, creatorId: self.creatorId, participants: self.participants, image: image)
+        TemporaryMeetingInfo(title: title, description: description, participants: self.participants, image: image)
     }
     
     func setInvitedFriends(_ participants: [UInt64]) -> Self {
-        TemporaryMeetingInfo(title: self.title, description: self.description, creatorId: self.creatorId, participants: participants, image: self.image)
+        TemporaryMeetingInfo(title: self.title, description: self.description, participants: participants, image: self.image)
     }
 }
