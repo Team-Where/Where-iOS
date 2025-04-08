799d4b5a83ea36b7d7c4a6781b720e65afd00392
bamsak <qjatjrdl963@naver.com>
2025-04-09 00:09:55 +0900

    [Feat] 공지 삭제 DTO 구현

--- Where/Service/Network/DTO/DeleteAnnouncementDTO.swift
+++ Where/Service/Network/DTO/DeleteAnnouncementDTO.swift
@@ -5,4 +5,13 @@
 //  Created by BOMBSGIE on 4/9/25.
 //
 
-import Foundation
+/// 공지 삭제
+enum DeleteAnnouncementDTO {
+    struct Request: Encodable {
+        let announcementID: UInt64
+        
+        enum CodingKeys: String, CodingKey {
+            case announcementID = "id"
+        }
+    }
+}
