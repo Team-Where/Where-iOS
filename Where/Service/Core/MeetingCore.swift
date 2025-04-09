fd74fb1124648754a1fcc0ff73de99a794ff4056
YunSwain <destap@naver.com>
2025-04-09 21:55:34 +0900

    [Refactor]: MeetingCore 실구현체 구현 (WIP)

--- Where/Service/Core/MeetingCore.swift
+++ Where/Service/Core/MeetingCore.swift
@@ -5,16 +5,14 @@
 //  Created by Swain Yun on 4/7/25.
 //
 
-import Foundation
+import SwiftUI
 import Combine
 
 protocol MeetingCoreProtocol {
     /// 나와 연관된 모임 목록
-    var meetings: AnyPublisher<[UInt64: Meeting], CommunityCoreError> { get }
+    var meetings: AnyPublisher<[UInt64: Meeting], MeetingCoreError> { get }
     /// 최근 살펴본 모임 정보
     var currentMeeting: AnyPublisher<Meeting?, Never> { get }
-    /// 사용자 식별자
-    var userId: UInt64? { get }
     
     /// 모임 일정 등록
     /// - Parameters:
@@ -73,4 +71,109 @@ protocol MeetingCoreProtocol {
 
 enum MeetingCoreError: Error {
     
+}
+
+final class MeetingCore {
+    @Published private var _meetings = [UInt64: Meeting]()
+    @Published private var _currentMeeting: Meeting?
+    
+    private var userID: UInt64?
+    
+    private let authCore: AuthentificationCoreProtocol
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(authCore: AuthentificationCoreProtocol) {
+        self.authCore = authCore
+        subscribe()
+    }
+    
+    private func subscribe() {
+        authCore.user
+            .sink { completion in
+                switch completion {
+                case .finished: break
+                case .failure(let error):
+                    #if DEBUG
+                    print(error)
+                    #endif
+                }
+            } receiveValue: { [weak self] user in
+                guard let id = user?.id else {
+                    self?._meetings = [:]
+                    self?._currentMeeting = nil
+                    self?.userID = nil
+                    return
+                }
+                self?.userID = id
+                self?.readMeetings()
+            }
+            .store(in: &cancellables)
+    }
+}
+
+// MARK: - MeetingCoreProtocol Confirmation
+extension MeetingCore: MeetingCoreProtocol {
+    var meetings: AnyPublisher<[UInt64: Meeting], MeetingCoreError> {
+        $_meetings
+            .map { $0 }
+            .setFailureType(to: MeetingCoreError.self)
+            .eraseToAnyPublisher()
+    }
+    
+    var currentMeeting: AnyPublisher<Meeting?, Never> {
+        $_currentMeeting
+            .eraseToAnyPublisher()
+    }
+    
+    func createSchedule(id: UInt64) {
+        
+    }
+    
+    func readSchedule(id: UInt64) {
+        
+    }
+    
+    func updateSchedule(id: UInt64) {
+        
+    }
+    
+    func deleteSchedule(id: UInt64) {
+        
+    }
+    
+    func createMeeting(info: TemporaryMeetingInfo) {
+        
+    }
+    
+    func readMeetings() {
+        
+    }
+    
+    func updateMeeting(id: UInt64, title: String?, description: String?, image: UIImage?) {
+        
+    }
+    
+    func endMeeting(id: UInt64) {
+        
+    }
+    
+    func exitMeeting(id: UInt64) {
+        
+    }
+    
+    func readParticipants(id: UInt64) {
+        
+    }
+    
+    func inviteParticipant(id: UInt64, participantId: UInt64) {
+        
+    }
+    
+    func acceptInvitation(id: UInt64) {
+        
+    }
+    
+    func readCurrentMeeting(id: UInt64) {
+        
+    }
 }
