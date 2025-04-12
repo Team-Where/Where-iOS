0b5b3c87770426038628abefa655d38444771325
YunSwain <destap@naver.com>
2025-04-12 22:29:10 +0900

    [Refactor]: CommentViewModel 코드 이전 등

--- Where/Feature/Meeting/Place/Comment/ViewModel/CommentViewModel.swift
+++ Where/Feature/Meeting/Place/Comment/ViewModel/CommentViewModel.swift
@@ -11,8 +11,37 @@ import Combine
 final class CommentViewModel: ObservableObject {
     @Published var sheetType: SheetType?
     @Published var comments = [Comment]()
-    @Published var myComment: Comment?
+    @Published var currentComment: Comment?
     @Published var commentTextField = String()
+    private var userID: UInt64?
+    private var currentPlace: Place?
+    
+    private let placeCore: PlaceCoreProtocol
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(
+        placeCore: PlaceCoreProtocol
+    ) {
+        self.placeCore = placeCore
+        subscribe()
+    }
+    
+    private func subscribe() {
+        placeCore.currentPlace
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .finished: break
+                case .failure(let error):
+                    #if DEBUG
+                    print(error)
+                    #endif
+                }
+            } receiveValue: { [weak self] place in
+                self?.currentPlace = place
+            }
+            .store(in: &cancellables)
+    }
 }
 
 // MARK: - Nested Types
@@ -21,7 +50,9 @@ extension CommentViewModel {
     enum SheetType: Identifiable {
         /// 코멘트 남기기(생성)
         case create
-        /// 코멘트 내용 확인, 삭제, 수정
+        /// 코멘트 내용 확인, 삭제
+        case read(comment: Comment)
+        /// 코멘트 수정
         case edit
         
         var id: String { String(describing: self) }
@@ -32,5 +63,30 @@ extension CommentViewModel {
 extension CommentViewModel {
     func presentCreationSheet() {
         sheetType = .create
+    }
+    
+    func createComment() {
+        guard let currentPlace else { return }
+        placeCore.createComment(placeID: currentPlace.id, description: commentTextField)
+    }
+    
+    func presentReadingSheet(comment: Comment) {
+        currentComment = comment
+        sheetType = .read(comment: comment)
+    }
+    
+    func deleteComment(_ comment: Comment) {
+        placeCore.deleteComment(id: comment.placeId)
+    }
+    
+    func presentEditingSheet() {
+        guard let currentComment else { return }
+        commentTextField = currentComment.description
+        sheetType = .edit
+    }
+    
+    func editComment() {
+        guard let currentComment else { return }
+        placeCore.updateComment(id: currentComment.placeId, description: commentTextField)
     }
 }
