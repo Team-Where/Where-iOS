af765e2cd6fe11f73717fa76cd933686572ded63
YunSwain <destap@naver.com>
2025-04-17 14:37:31 +0900

    [Refactor]: 친구 모델 추가로 인한 수정

--- Where/Service/Core/CommunityCore.swift
+++ Where/Service/Core/CommunityCore.swift
@@ -10,10 +10,10 @@ import Combine
 
 protocol CommunityCoreProtocol {
     /// 나의 친구 목록
-    var friends: AnyPublisher<[UInt64: User], CommunityCoreError> { get }
+    var friends: AnyPublisher<[UInt64: FriendRelationship], CommunityCoreError> { get }
     
     /// 친구 추가
-    func createFriend(friend: User)
+    func createFriend(friend: FriendRelationship)
     /// 친구 목록 조회
     func readFriends()
     /// 친구 삭제
@@ -31,13 +31,13 @@ enum CommunityCoreError: Error {
 }
 
 final class CommunityCore {
-    @Published private(set) var _friends: [UInt64: User] = [:]
+    @Published private(set) var _friends: [UInt64: FriendRelationship] = [:]
     
     private var userID: UInt64?
     
     private let tokenStorage: TokenStorageProtocol
     private let auth: AuthentificationCoreProtocol
-    private let friendsSubject = CurrentValueSubject<[UInt64: User], CommunityCoreError>([:])
+    private let friendsSubject = CurrentValueSubject<[UInt64: FriendRelationship], CommunityCoreError>([:])
     private var cancellables = Set<AnyCancellable>()
     
     init(
@@ -69,16 +69,19 @@ final class CommunityCore {
 
 // MARK: CommunityCoreProtocol Confirmation
 extension CommunityCore: CommunityCoreProtocol {
-    var friends: AnyPublisher<[UInt64 : User], CommunityCoreError> {
+    var friends: AnyPublisher<[UInt64 : FriendRelationship], CommunityCoreError> {
         friendsSubject.eraseToAnyPublisher()
     }
     
-    func createFriend(friend: User) {
+    func createFriend(friend: FriendRelationship) {
         
     }
     
     func readFriends() {
-        
+        Task { @MainActor in
+            _friends = PreviewHelper.shared.mockFriends.reduce(into: [:]) { $0[$1.id] = $1 }
+            friendsSubject.send(_friends)
+        }
     }
     
     func deleteFriend(id: UInt64) {
