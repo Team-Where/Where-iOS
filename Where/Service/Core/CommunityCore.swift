2a3fe90b3f710fbdf5477db1390da10776787283
YunSwain <destap@naver.com>
2025-04-19 00:27:11 +0900

    [Refactor]: 코드 정리

--- Where/Service/Core/CommunityCore.swift
+++ Where/Service/Core/CommunityCore.swift
@@ -14,29 +14,37 @@ protocol CommunityCoreProtocol: CoreProtocol {
     
     /// 친구 추가
     func createFriend(friend: FriendRelationship)
-    /// 친구 목록 조회
-    func readFriends()
+    /// 특정 친구 조회
+    func fetchFriend(id: UInt64) -> FriendRelationship?
     /// 친구 삭제
-    /// - Parameters:
-    ///     - id: 삭제할 대상의 고유 식별자
     func deleteFriend(id: UInt64)
     /// 친구 즐겨찾기 토글
-    /// - Parameters:
-    ///     - id: 즐겨찾기 설정 또는 해제할 대상의 고유 식별자
     func toggleBookmarkFriend(id: UInt64)
 }
 
-enum CommunityCoreError: Error {
+protocol CommunityMediationProtocol {
+    /// 현재 사용자의 친구 목록 로드를 지시, 중재자에 의해 호출됨
+    /// - Parameters:
+    ///     - 친구 목록을 로드할 사용자 식별자
+    func loadFriends()
     
+    /// 현재 사용자 식별자를 설정, 중재자에 의해 호출됨
+    func setCurrentUserID(_ id: UInt64)
+}
+
+enum CommunityCoreError: Error {
+    case networkingError
 }
 
 final class CommunityCore {
-    @Published private(set) var _friends: [UInt64: FriendRelationship] = [:]
-    
     weak var mediator: CoreMediatorProtocol?
     
-    private let tokenStorage: TokenStorageProtocol
+    private var _friends = [UInt64: FriendRelationship]()
+    
     private let friendsSubject = CurrentValueSubject<[UInt64: FriendRelationship], CommunityCoreError>([:])
+    
+    private var currentUserID: UInt64?
+    private let tokenStorage: TokenStorageProtocol
     private var cancellables = Set<AnyCancellable>()
     
     init(
@@ -52,6 +60,9 @@ final class CommunityCore {
                 // TODO: 에러 핸들링 강화
             } receiveValue: { [weak self] dict in
                 self?._friends = dict
+                
+                guard let userID = self?.currentUserID else { return }
+                self?.mediator?.notify(event: .friendListUpdated(id: userID))
             }
             .store(in: &cancellables)
     }
@@ -67,11 +78,8 @@ extension CommunityCore: CommunityCoreProtocol {
         
     }
     
-    func readFriends() {
-        Task { @MainActor in
-            _friends = PreviewHelper.shared.mockFriends.reduce(into: [:]) { $0[$1.id] = $1 }
-            friendsSubject.send(_friends)
-        }
+    func fetchFriend(id: UInt64) -> FriendRelationship? {
+        _friends[id]
     }
     
     func deleteFriend(id: UInt64) {
@@ -80,5 +88,16 @@ extension CommunityCore: CommunityCoreProtocol {
     
     func toggleBookmarkFriend(id: UInt64) {
         
+    }
+}
+
+// MARK: - CommunityMediationProtocol Conformation
+extension CommunityCore: CommunityMediationProtocol {
+    func loadFriends() {
+        
+    }
+    
+    func setCurrentUserID(_ id: UInt64) {
+        currentUserID = id
     }
 }
