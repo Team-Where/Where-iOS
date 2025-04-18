647b84fe0fa171160084bc509caa9cf8b37877e6
YunSwain <destap@naver.com>
2025-04-18 23:52:32 +0900

    [Move]: CoreEvent 폴더링 이동

--- Where/Service/Core/CoreMediator.swift
+++ Where/Service/Core/CoreMediator.swift
@@ -9,37 +9,6 @@ import Foundation
 
 typealias CoreMediatorProtocol = CoreLinkageProtocol & Notifiable
 
-// TODO: 중재 이벤트 추가하기
-enum CoreEvent {
-    // MARK: - Authentification Related
-    case userDidLogin(id: UInt64)
-    case userDidLogout(id: UInt64)
-    
-    // MARK: - Community Related
-    case friendListUpdated(id: UInt64)
-    
-    // MARK: - Meeting Related
-    case meetingCreated(id: UInt64)
-    case meetingUpdated(id: UInt64)
-    case currentMeetingDidChange(id: UInt64)
-    case participantListUpdated(id: UInt64)
-    
-    // MARK: - Place Related
-    case placeSelected(id: UInt64)
-    case placeCreated(meetingID: UInt64, id: UInt64)
-    case commentCreated(placeID: UInt64, id: UInt64)
-    
-    // MARK: - Support Related
-    case inquiryCreated(userID: UInt64, id: UInt64)
-    case adminInquiryReplyCreated(id: UInt64, replyID: UInt64)
-    case announcementCreated(id: UInt64)
-    case announcementUpdated(id: UInt64)
-    case announcementDeleted(id: UInt64)
-    case faqCreated(id: UInt64)
-    case faqUpdated(id: UInt64)
-    case faqDeleted(id: UInt64)
-}
-
 protocol CoreLinkageProtocol: AnyObject {
     func attachAuthentificationCore(_ core: AuthentificationCoreProtocol)
     func attachCommunityCore(_ core: CommunityCoreProtocol)
