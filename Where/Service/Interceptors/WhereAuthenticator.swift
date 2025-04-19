c55c99b67bd28213f48ce379b9866e19901fa8fc
bamsak <qjatjrdl963@naver.com>
2025-04-19 23:51:04 +0900

    [Refactor] 기존 RequestInterceptor 방식에서 Authenticator 채택 방식으로 변경

---
+++ Where/Service/Interceptors/WhereAuthenticator.swift
@@ -0,0 +1,82 @@
+//
+//  WhereAuthenticator.swift
+//  Where
+//
+//  Created by Swain Yun on 3/31/25.
+//
+
+import Foundation
+import Alamofire
+
+final class WhereAuthenticator {
+    private let key: UInt64
+    private let tokenStorage: TokenStorageProtocol
+    private let encoder: JSONEncoder
+    
+    init(
+        key: UInt64,
+        tokenStorage: TokenStorageProtocol,
+        encoder: JSONEncoder
+    ) {
+        self.key = key
+        self.tokenStorage = tokenStorage
+        self.encoder = encoder
+    }
+}
+
+extension WhereAuthenticator: Authenticator {
+    typealias Credential = Tokens
+    func apply(_ credential: Tokens, to urlRequest: inout URLRequest) {
+        urlRequest.headers.add(.authorization(bearerToken: credential.accessToken))
+    }
+    
+    func didRequest(_ urlRequest: URLRequest, with response: HTTPURLResponse, failDueToAuthenticationError error: any Error)
+    -> Bool
+    {
+        return response.statusCode == 401
+    }
+    
+    func isRequest(_ urlRequest: URLRequest, authenticatedWith credential: Tokens) -> Bool {
+        let bearerToken = HTTPHeader.authorization(bearerToken: credential.accessToken).value
+        return urlRequest.headers[Tokens.HeaderKey.accseeToken.rawValue] == bearerToken
+    }
+    
+    func refresh(
+        _ credential: Tokens, for session: Alamofire.Session, completion: @escaping @Sendable (Result<Tokens, any Error>) -> Void
+    ) {
+        
+        WithoutTokenAPIServiceProvider()
+            .makeProvider(Endpoint.self)
+            .request(.reissueAccessToken(refreshToken: credential.refreshToken)) { [weak self] result in
+                switch result {
+                case .success(let response):
+                    guard let access = response.response?.headers[Tokens.HeaderKey.accseeToken.rawValue]?.split(separator: " ").last as? String
+                    else {
+                        return completion(.failure(AuthenticatorError.tokenNotFound))
+                    }
+                    let newToken = Tokens(accessToken: access, refreshToken: credential.refreshToken)
+                    
+                    do {
+                        try self?.saveToken(newToken)
+                    } catch {
+                        completion(.failure(AuthenticatorError.saveTokenFailed))
+                    }
+                    completion(.success(newToken))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+}
+
+private extension WhereAuthenticator {
+    enum AuthenticatorError: Error {
+        case saveTokenFailed
+        case tokenNotFound
+    }
+    
+    func saveToken(_ token: Credential) throws {
+        let encodedData = try encoder.encode(token)
+        try tokenStorage.store(encodedData, by: key)
+    }
+}
