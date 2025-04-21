596aba1c51993ff4549db67578efa78a1e5873f5
bamsak <qjatjrdl963@naver.com>
2025-04-21 23:03:04 +0900

    [Refactor] MoyaProvider를 활용한 Network 계층 추상화
    - protocol로 추상화하여 실 구현체에서의 의존성 제거

--- Where/Assembly/ServiceAssembly.swift
+++ Where/Assembly/ServiceAssembly.swift
@@ -14,5 +14,26 @@ struct ServiceAssembly: Assembly {
             TokenStorage()
         }
         .inObjectScope(.container)
+        
+        container.register(JSONDecoder.self) { _ in
+            JSONDecoder()
+        }
+        .inObjectScope(.container)
+        
+        container.register(JSONEncoder.self) { _ in
+            JSONEncoder()
+        }
+        .inObjectScope(.container)
+        
+        container.register(APIServable.self) { resolver in
+            guard let decoder = resolver.resolve(JSONDecoder.self),
+                  let encoder = resolver.resolve(JSONEncoder.self),
+                  let tokenStorage = resolver.resolve(TokenStorageProtocol.self)
+            else {
+                fatalError("JSONDecoder Not Initialized")
+            }
+            return APIService(decoder, encoder, tokenStorage)
+        }
+        .inObjectScope(.container)
     }
 }
