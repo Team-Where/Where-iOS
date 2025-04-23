dbbc8f81783ee2bb5f69c894dbdfa4ecc0205259
YunSwain <destap@naver.com>
2025-04-23 21:33:10 +0900

    [Refactor]: NotificationCore 구현 중 (WIP)

--- Where/Entities/Notification.swift
+++ Where/Entities/Notification.swift
@@ -17,4 +17,36 @@ struct Notification: Identifiable {
     let content: String
     /// 알림 발행 시간
     let date: Date
+    /// 알림 읽음 여부
+    let isRead: Bool
+    /// 알림 종류
+    let type: NotificationType
+}
+
+/// 알림 종류
+enum NotificationType {
+    /// 장소 추가
+    case placeAdded
+    /// 장소 삭제
+    case placeDeleted
+    /// 장소 Pick
+    case placePicked
+    /// 장소 좋아요수 변경
+    case placeLikesUpdated
+    /// 같이 찾은 장소
+    case placeSimulaneouslyPicked
+    /// 일정 추가
+    case scheduleAdded
+    /// 일정 변경
+    case scheduleUpdated
+    /// 일정 삭제
+    case scheduleDeleted
+    /// 코멘트 추가
+    case commentAdded
+    /// 코멘트 수정
+    case commentUpdated
+    /// 코멘트 삭제
+    case commentDeleted
+    /// 모임 참가신청 수락
+    case meetingInvitationAccepted
 }
--- Where/Service/Core/NotificationCore.swift
+++ Where/Service/Core/NotificationCore.swift
@@ -6,11 +6,65 @@
 //
 
 import Foundation
+import Combine
 
 protocol NotificationCoreProtocol: CoreProtocol {
+    /// 알림 목록
+    var notifications: AnyPublisher<[UInt64: Notification], NotificationCoreError> { get }
     
+    /// 알림 읽음 처리
+    func markNotificationAsRead(id: UInt64)
 }
 
 protocol NotificationMediationProtocol {
+    /// 수신된 알림 저장을 지시, 중재자에 의해 호출됨
+    func saveNotification(_ notification: Notification)
+}
+
+enum NotificationCoreError: Error {
     
+}
+
+final class NotificationCore {
+    weak var mediator: Notifiable?
+    
+    private var _notifications = [UInt64: Notification]()
+    
+    private let notificationsSubject = CurrentValueSubject<[UInt64: Notification], NotificationCoreError>([:])
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        subscribe()
+    }
+    
+    private func subscribe() {
+        notificationsSubject
+            .sink { completion in
+                // TODO: 에러 핸들링 강화
+            } receiveValue: { [weak self] dict in
+                self?._notifications = dict
+            }
+            .store(in: &cancellables)
+    }
+}
+
+// MARK: - NotificationCoreProtocol Conformation
+extension NotificationCore: NotificationCoreProtocol {
+    var notifications: AnyPublisher<[UInt64 : Notification], NotificationCoreError> {
+        notificationsSubject.eraseToAnyPublisher()
+    }
+    
+    func markNotificationAsRead(id: UInt64) {
+        guard let notification =  notificationsSubject.value[id] else { return }
+        _notifications[id] = Notification(
+            id: notification.id,
+            title: notification.title,
+            content: notification.content,
+            date: notification.date,
+            isRead: true,
+            type: notification.type
+        )
+        notificationsSubject.send(_notifications)
+    }
 }
