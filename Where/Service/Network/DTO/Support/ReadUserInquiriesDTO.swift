759e6f2b381462d7f896b57d97b9a3e616780799
YunSwain <destap@naver.com>
2025-04-25 00:28:16 +0900

    [Refactor]: API 응답 스펙 변경에 따른 수정

--- Where/Service/Network/DTO/Support/ReadUserInquiriesDTO.swift
+++ Where/Service/Network/DTO/Support/ReadUserInquiriesDTO.swift
@@ -5,6 +5,8 @@
 //  Created by BOMBSGIE on 4/8/25.
 //
 
+import Foundation
+
 /// 1:1 문의 조회 - 일반 사용자
 enum ReadUserInquiriesDTO {
     struct Request: Encodable {
@@ -25,14 +27,29 @@ extension ReadUserInquiriesDTO {
         let imageURLStrings: [String]?
         let isAnswered: Bool
         let answerContent: String?
+        let modifiedAt: Date
+        let answeredAt: Date?
         
         enum CodingKeys: String, CodingKey {
             case inquiryID = "id"
             case isAnswered = "answerd"
             case imageURLStrings = "images"
+            case modifiedAt = "inquiryDate"
+            case answeredAt = "answerDate"
             case title, content, answerContent
         }
+        
+        func toEntity() -> Inquiry {
+            .init(
+                id: inquiryID,
+                modifiedAt: .now,
+                title: title,
+                content: content,
+                imageURLs: (imageURLStrings ?? []).compactMap { URL(string: $0) },
+                isAnswered: isAnswered,
+                answerContent: answerContent
+            )
+        }
     }
-    
 }
 
