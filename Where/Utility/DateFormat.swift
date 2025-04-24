cdd94e1632e2c837e557f2a1b09009758217be83
bamsak <qjatjrdl963@naver.com>
2025-04-25 00:32:48 +0900

    [Refactor] 밀리초 포함 서버 시간 DateFormatter 기능 추가

--- Where/Utility/DateFormat.swift
+++ Where/Utility/DateFormat.swift
@@ -81,6 +81,9 @@ enum DateFormat: String {
     
     /// 서버 날짜, 시간 (년-월-일 시:분:초)
     case serverDateTime2 = "yyyy-MM-dd HH:mm:ss"
+    
+    /// 서버 날짜, 시간 밀리초 (년-월-일 시:분:초.밀리초)
+    case serverDateTimeWithMS = "yyyy-MM-dd HH:mm:ss.SSS"
 }
 
 // MARK: DateFormatter 관련
