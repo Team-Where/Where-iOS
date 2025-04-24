5e7c445d95aa384e00ad0375622543211d4301fb
YunSwain <destap@naver.com>
2025-04-24 22:32:04 +0900

    [Feat]: HistoryReminderViewModel 추가

---
+++ Where/Feature/HistoryReminder/ViewModel/HistoryReminderViewModel.swift
@@ -0,0 +1,66 @@
+//
+//  HistoryReminderViewModel.swift
+//  Where
+//
+//  Created by Swain Yun on 4/24/25.
+//
+
+import Foundation
+import Combine
+
+final class HistoryReminderViewModel: ObservableObject {
+    @Published private(set) var yearGroups = [YearGroup]()
+    
+    private let meetingCore: MeetingCoreProtocol
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(meetingCore: MeetingCoreProtocol) {
+        self.meetingCore = meetingCore
+        subscribe()
+    }
+    
+    private func subscribe() {
+        meetingCore.meetingSummaries
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                
+            } receiveValue: { [weak self] dict in
+                let calendar = Calendar.current
+                var grouped = [Int: [Int: [MeetingSummary]]]()
+                dict.values.forEach {
+                    let year = calendar.component(.year, from: $0.finishedAt)
+                    let month = calendar.component(.month, from: $0.finishedAt)
+                    grouped[year, default: [:]][month, default: []].append($0)
+                }
+                let sortedGroups = grouped
+                    .sorted(by: { $0.key > $1.key })
+                    .map { (year, monthsDict) in
+                        let monthGroups = monthsDict
+                            .sorted(by: { $0.key > $1.key })
+                            .map { (month, meetings) in
+                                let sortedMeetings = meetings.sorted(by: { $0.finishedAt > $1.finishedAt })
+                                return MonthGroup(month: month, meetings: sortedMeetings)
+                            }
+                        return YearGroup(year: year, months: monthGroups)
+                    }
+                
+                self?.yearGroups = sortedGroups
+            }
+            .store(in: &cancellables)
+    }
+}
+
+// MARK: - Nested Types
+extension HistoryReminderViewModel {
+    struct MonthGroup: Identifiable {
+        let id = UUID()
+        let month: Int
+        let meetings: [MeetingSummary]
+    }
+    
+    struct YearGroup: Identifiable {
+        let id = UUID()
+        let year: Int
+        let months: [MonthGroup]
+    }
+}
