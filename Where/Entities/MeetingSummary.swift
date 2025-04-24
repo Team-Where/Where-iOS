836657f11ae4ed70c55f14fd451946d38d238d99
YunSwain <destap@naver.com>
2025-04-24 19:36:00 +0900

    [Feat]: 친구와 함께한 모임 요약정보 도메인 모델 추가

---
+++ Where/Entities/MeetingSummary.swift
@@ -0,0 +1,20 @@
+//
+//  MeetingSummary.swift
+//  Where
+//
+//  Created by Swain Yun on 4/23/25.
+//
+
+import Foundation
+
+struct MeetingSummary: Hashable {
+    let id: UInt64
+    let title: String
+    let description: String
+    let imageURL: URL?
+    let finishedAt: Date
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
