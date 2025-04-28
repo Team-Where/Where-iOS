ee930d3900df9d6605d9cb44bf9044e70081d0fc
YunSwain <destap@naver.com>
2025-04-28 23:25:21 +0900

    [Feat]:

--- Where/Feature/Support/View/AnnouncementView.swift
+++ Where/Feature/Support/View/AnnouncementView.swift
@@ -6,25 +6,28 @@
 //
 
 import SwiftUI
+import Swinject
+
+fileprivate typealias NavigationType = AnnouncementViewModel.NavigationType
 
 struct AnnouncementView: View {
-    @State private var navigationType: NavigationType?
-    @State private var announcements: [Announcement] = [
-        .init(id: 0, title: "[이벤트] <신규가입 이벤트> 당첨자 발표", content: "안녕하세요 어디 서비스 운영자입니다.\n\n202년 신규가입 이벤트 당첨자를 발표합니다.\n\n<당첨자 발표>\n\n> 냠냠쩝쩝님\n> 이초홍님\n> 진키님\n\n축하드립니다!\n앱을 사용하시면서 불편하신 사항이 있으시다면 어디앱 메뉴에서 [1:1문의]를 이용해주세요.\n감사합니다.", date: .now, type: .common),
-        .init(id: 1, title: "[공지] 신규 기능 업데이트 안내", content: "ㅁㄴㅇㄹ", date: .now, type: .common),
-        .init(id: 2, title: "[공지] 서버 점검에 따른 서비스 일시 중단 안내", content: "ㄹㅇㄹㅁㄴㅇㄹ", date: .now, type: .common)
-    ]
+    @ObservedObject private var viewModel: AnnouncementViewModel
+    
+    init(resolver: Resolver) {
+        self.viewModel = resolver.resolve(AnnouncementViewModel.self)!
+    }
     
     var body: some View {
         ScrollView(.vertical) {
             LazyVStack {
-                ForEach(announcements) { announcement in
+                ForEach(viewModel.announcements) { announcement in
                     Cell(announcement)
                 }
             }
         }
         .navigationBarBackButtonHidden()
         .navigationBarTitleDisplayMode(.inline)
+        .toolbar(.hidden, for: .tabBar)
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
                 BackButton()
@@ -37,16 +40,15 @@ struct AnnouncementView: View {
             }
             
             // MARK: 관리자 권한인지 판단해서 노출할 수 있도록 수정해야함
-            ToolbarItem(placement: .topBarTrailing) {
-                Button {
-                    navigationType = .editAnnouncement
-                } label: {
-                    Image(systemName: "pencil.line")
-                        .foregroundStyle(.where(.gray800))
-                }
-            }
+//            ToolbarItem(placement: .topBarTrailing) {
+//                Button {
+//                    navigationType = .editAnnouncement
+//                } label: {
+//                    Image(systemName: "pencil.line")
+//                        .foregroundStyle(.where(.gray800))
+//                }
+//            }
         }
-        .toolbar(.hidden, for: .tabBar)
     }
 }
 
@@ -77,20 +79,5 @@ extension AnnouncementView {
             }
             .disclosureGroupStyle(WhereDisclosureGroupStyle(labelHeight: 74))
         }
-    }
-}
-
-// MARK: NavigationType
-extension AnnouncementView {
-    /// 공지사항 화면에서 라우팅 가능한 네비게이션패스의 종류
-    enum NavigationType: Hashable {
-        /// FAQ 및 공지사항 작성 화면
-        case editAnnouncement
-    }
-}
-
-#Preview {
-    NavigationStack {
-        AnnouncementView()
     }
 }
---
+++ Where/Feature/Support/ViewModel/AnnouncementViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  AnnouncementViewModel.swift
+//  Where
+//
+//  Created by Swain Yun on 4/28/25.
+//
+
+import Foundation
+import Combine
+
+final class AnnouncementViewModel: ObservableObject {
+    @Published var navigationType: NavigationType?
+    @Published var announcements = [Announcement]()
+    
+    private let supportCore: SupportCoreProtocol
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(supportCore: SupportCoreProtocol) {
+        self.supportCore = supportCore
+        subscribe()
+    }
+    
+    private func subscribe() {
+        supportCore.announcements
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                // TODO: 에러 핸들링
+            } receiveValue: { [weak self] dict in
+                self?.announcements = dict.values.sorted { $0.date > $1.date }
+            }
+            .store(in: &cancellables)
+    }
+}
+
+// MARK: - Nested Types
+extension AnnouncementViewModel {
+    /// 공지사항 화면에서 라우팅 가능한 네비게이션패스의 종류
+    enum NavigationType: Hashable {
+        /// FAQ 및 공지사항 작성 화면
+        case editAnnouncement
+    }
+}
