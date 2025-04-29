696b8b00d65ef7929325ca2d08463f29414a1e31
YunSwain <destap@naver.com>
2025-04-30 02:39:30 +0900

    [Feat]: 지도 앱 딥링크 빌더 추가

---
+++ Where/Entities/MapAppScheme.swift
@@ -0,0 +1,39 @@
+//
+//  MapAppScheme.swift
+//  Where
+//
+//  Created by Swain Yun on 4/30/25.
+//
+
+import UIKit
+
+/// URL Schemes 및 관련 정보를 정의합니다.
+enum MapAppScheme {
+    case kakaomap
+    /// 네이버지도
+    case navermap
+    
+    private var identifier: String? { Bundle.main.bundleIdentifier }
+    
+    var baseURLString: String {
+        switch self {
+        case .kakaomap: "kakaomap://"
+        case .navermap: "nmap://"
+        }
+    }
+    
+    func isAppInstalled() -> Bool {
+        guard let url = URL(string: baseURLString) else { return false }
+        return UIApplication.shared.canOpenURL(url)
+    }
+    
+    func openURL() -> URL? {
+        switch self {
+        case .kakaomap:
+            return URL(string: "\(baseURLString)open?page=placeSearch")
+        case .navermap:
+            guard let identifier = identifier else { return nil }
+            return URL(string: "\(baseURLString)map?&appname=\(identifier)")
+        }
+    }
+}
