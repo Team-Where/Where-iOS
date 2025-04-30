9662e4421d2f599f0f4ddd9db8103434a747117e
YunSwain <destap@naver.com>
2025-04-30 23:50:34 +0900

    [Feat]: ContentViewModel 추가 및 코드 이전

---
+++ Where/Feature/Main/View/ContentView.swift
@@ -0,0 +1,77 @@
+//
+//  ContentView.swift
+//  Where
+//
+//  Created by 이현호 on 1/5/25.
+//
+
+import SwiftUI
+import Swinject
+
+fileprivate typealias TabItem = ContentViewModel.TabItem
+
+struct ContentView: View {
+    @ObservedObject private var viewModel: ContentViewModel
+    
+    private let resolver: Resolver
+    
+    init(resolver: Resolver) {
+        self.resolver = resolver
+        self.viewModel = resolver.resolve(ContentViewModel.self)!
+    }
+    
+    var body: some View {
+        TabView(selection: $viewModel.selectedTab) {
+            // 내모임 뷰
+            NavigationStack {
+                MyMeetingView(resolver: resolver)
+            }
+            .tabItem {
+                Label("내 모임", systemImage: "person.2")
+                    .environment(\.symbolVariants, .none)
+            }
+            .tag(TabItem.myMeeting)
+            
+            // 새 모임 만들기
+            Color.clear
+                .tabItem {
+                    Image("BottomPlus")
+                }
+                .tag(TabItem.createMeeting)
+            
+            // 친구목록 뷰
+            NavigationStack {
+                FriendsListView(resolver: resolver)
+            }
+            .tabItem {
+                Label("친구목록", systemImage: "list.bullet")
+            }
+            .tag(TabItem.friendsList)
+        }
+        .tint(.black) // 선택된 탭 아이템의 색상
+        .onChange(of: viewModel.selectedTab) { _, newTab in
+            viewModel.onChange(newTab)
+        }
+        .sheet(isPresented: $viewModel.isCreateMeetingSheetPresented) {
+            CreateMeetingView(resolver: resolver)
+                .presentationCornerRadius(24)
+                .presentationDetents([.fraction(0.99)])
+        }
+        .fullScreenCover(item: $viewModel.fullScreenCoverType) { type in
+            switch type {
+            case .completeCreation(let meeting):
+                EmptyView()
+//                CompleteCreationView(isCompleteCreationViewPresented: <#T##Binding<Bool>#>, meeting: <#T##Meeting#>)
+            case .login:
+                EmptyView()
+//                LoginView(<#T##isPresented: Binding<Bool>##Binding<Bool>#>, resolver: <#T##any Resolver#>)
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ContentView(resolver: PreviewHelper.shared.resolver)
+    }
+}
---
+++ Where/Feature/Main/ViewModel/ContentViewModel.swift
@@ -0,0 +1,70 @@
+//
+//  ContentViewModel.swift
+//  Where
+//
+//  Created by Swain Yun on 4/30/25.
+//
+
+import Foundation
+import Combine
+import Swinject
+
+final class ContentViewModel: ObservableObject {
+    @Published var selectedTab: TabItem = .myMeeting
+    @Published var previousTab: TabItem = .myMeeting
+    @Published var isCreateMeetingSheetPresented = false
+    @Published var fullScreenCoverType: FullScreenCoverType?
+    
+    private let authCore: AuthentificationCoreProtocol
+    private let meetingCore: MeetingCoreProtocol
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(resolver: Resolver) {
+        self.authCore = resolver.resolve(AuthentificationCoreProtocol.self)!
+        self.meetingCore = resolver.resolve(MeetingCoreProtocol.self)!
+        subscribe()
+    }
+    
+    private func subscribe() {
+        // TODO: 모임이 새로 생기면 화면 띄울 수 있게 구독하기
+    }
+}
+
+// MARK: Nested Types
+extension ContentViewModel {
+    /// 탭 뷰의 탭아이템 종류
+    enum TabItem: Hashable {
+        case myMeeting
+        case createMeeting
+        case friendsList
+    }
+    
+    /// 메인(루트) 화면에서 라우팅 가능한 풀스크린커버의 종류
+    enum FullScreenCoverType: Identifiable {
+        /// 모임 생성 완료 화면
+        case completeCreation(Meeting)
+        /// 로그인 화면
+        case login
+        
+        var id: String { String(describing: self) }
+    }
+}
+
+// MARK: - Interfaces
+extension ContentViewModel {
+    func onChange(_ newTab: TabItem) {
+        guard newTab == .createMeeting else {
+            return previousTab = newTab
+        }
+        
+        // 새 모임 만들기 탭이 선택 됐으면 로그인 필요한지 확인해야함
+        guard authCore.isLoginNeeded == false else {
+            // 비로그인 상황이라면 새 모임 만들기 진행 불가, 로그인 화면 등장
+            return fullScreenCoverType = .login
+        }
+        
+        // 로그인한 상황이라면 새 모임 만들기 진행 가능
+        isCreateMeetingSheetPresented = true
+        selectedTab = previousTab
+    }
+}
