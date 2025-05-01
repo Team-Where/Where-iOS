0a64361cfd0ca0ed6e32e05e19c322f619999cc9
bamsak <qjatjrdl963@naver.com>
2025-05-01 21:45:55 +0900

    [Feat] ViewModel로 meetingID분리 및 초대 현황 기능 연동

--- Where/Feature/Meeting/Invitation/ViewModel/InviteFriendsViewModel.swift
+++ Where/Feature/Meeting/Invitation/ViewModel/InviteFriendsViewModel.swift
@@ -15,6 +15,14 @@ final class InviteFriendsViewModel: ObservableObject {
     @Published var isFloaterPresented: Bool = false
     @Published var isSearching: Bool = false
     @Published var searchingText: String = String()
+    @Published var invitedFriends = [MeetingInvitationState]()
+    @Published var pendingFriends = [MeetingInvitationState]()
+    @Published private var _meetingID: UInt64!
+    
+    
+    var invitationStates: [MeetingInvitationState] {
+        invitedFriends + pendingFriends
+    }
     
     private let communityCore: CommunityCoreProtocol
     private let meetingCore: MeetingCoreProtocol
@@ -56,13 +64,33 @@ final class InviteFriendsViewModel: ObservableObject {
                 self?.friends = dict.values.sorted { $0.nickname < $1.nickname }
             }
             .store(in: &cancellables)
+        
+        meetingCore.invitationStatus
+            .mapError { ViewModelError.meetingError($0) }
+            .combineLatest($_meetingID.setFailureType(to: ViewModelError.self))
+            .compactMap { (dict, id) -> [MeetingInvitationState]? in
+                guard let id = id else { return nil }
+                return dict[id]
+            }
+            .sink { completion in
+                // TODO: 에러 핸들링
+            } receiveValue: { [weak self] status in
+                self?.invitedFriends = status.filter { $0.isInvited }
+                self?.pendingFriends = status.filter { !$0.isInvited }
+            }
+            .store(in: &cancellables)
+
     }
 }
 
 // MARK: - Interfaces
 extension InviteFriendsViewModel {
-    func inviteFriend(on meeting: Meeting, _ friend: FriendRelationship) {
+    func inviteFriend(_ friend: FriendRelationship) {
         isFloaterPresented.toggle()
-        meetingCore.inviteParticipant(id: meeting.id, guest: friend)
+        meetingCore.inviteParticipant(id: _meetingID, guest: friend)
+    }
+    
+    func setMeeting(id: UInt64) {
+        self._meetingID = id
     }
 }
