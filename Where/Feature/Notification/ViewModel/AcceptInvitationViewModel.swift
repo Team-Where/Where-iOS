2dcaa98b5a06f0c3bc1ca4b19eb0026f10320175
bamsak <qjatjrdl963@naver.com>
2025-05-05 22:46:20 +0900

    [Feat] 초대장 ViewModel 초기 구현 및 Core 연동

--- Where/Feature/Notification/ViewModel/AcceptInvitationViewModel.swift
+++ Where/Feature/Notification/ViewModel/AcceptInvitationViewModel.swift
@@ -6,8 +6,35 @@
 //
 
 import Foundation
+import Combine
 import Swinject
 
 final class AcceptInvitationViewModel: ObservableObject {
+    @Published var meeting: Meeting!
+    private let meetingCore: MeetingCoreProtocol
+    private var cancellables = Set<AnyCancellable>()
     
+    init(resolver: Resolver) {
+        meetingCore = resolver.resolve(MeetingCoreProtocol.self)!
+        subscribe()
+    }
+    
+    private func subscribe() {
+        meetingCore.invitedMeeting
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                // TODO: ErrorHanding
+            } receiveValue: { [weak self] in
+                self?.meeting = $0
+            }
+            .store(in: &cancellables)
+    }
+}
+
+extension AcceptInvitationViewModel {
+    // TODO: 화면 띄워지는 시점에서 초대 받은 모임 정보 불러오기 호출
+    func onApear(_ invitedCode: String) {
+        meetingCore.readMeetingDetailForInvitationLink(inviteCode: invitedCode)
+    }
+    // TODO: 유니버셜링크 연동 후, 초대 수락 구현
 }
