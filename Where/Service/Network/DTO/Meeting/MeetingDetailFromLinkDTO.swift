62ab69b848b8cf08986c23ecc819ebb23ecc748a
bamsak <qjatjrdl963@naver.com>
2025-05-05 22:43:45 +0900

    [Refactor] 초대장링크 모임정보 DTO 프로퍼티 타입 수정 및 Entity변환 로직 구현

--- Where/Service/Network/DTO/Meeting/MeetingDetailFromLinkDTO.swift
+++ Where/Service/Network/DTO/Meeting/MeetingDetailFromLinkDTO.swift
@@ -5,19 +5,34 @@
 //  Created by BOMBSGIE on 4/8/25.
 //
 
+import Foundation
+
 /// 모임 초대 조회 DTO
 enum MeetingDetailFromLinkDTO {
     struct Response: Decodable {
         let meetingID: UInt64
         let title: String
         let meetingImageURLString: String?
-        let scheduleDate: String
-        let scheduleTime: String
+        let scheduleDate: String?
+        let scheduleTime: String?
         
         enum CodingKeys: String, CodingKey {
             case meetingID = "meetingId"
             case meetingImageURLString = "image"
             case title, scheduleDate, scheduleTime
+        }
+        
+        func toEntity() -> Meeting {
+            return .init(
+                id: meetingID,
+                title: title,
+                description: "",
+                imageURL: URL(string: meetingImageURLString ?? ""),
+                createdAt: .now,
+                scheduleDate: scheduleDate?.toDate(by: .yyyyMMddHyphen),
+                scheduleTime: scheduleTime?.toDate(by: .HHmmss),
+                isFinished: false
+            )
         }
     }
 }
