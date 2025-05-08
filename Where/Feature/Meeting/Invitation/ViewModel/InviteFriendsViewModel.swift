16d717e5f4f912507e01743971e62dd93a44c226
YunSwain <destap@naver.com>
2025-05-08 23:56:06 +0900

    [Improve]: 구독 취소 로직으로 수정

--- Where/Feature/Meeting/Invitation/ViewModel/InviteFriendsViewModel.swift
+++ Where/Feature/Meeting/Invitation/ViewModel/InviteFriendsViewModel.swift
@@ -138,14 +138,15 @@ extension InviteFriendsViewModel {
 // MARK: - Interfaces
 extension InviteFriendsViewModel {
     func inviteFriend(_ friend: FriendRelationship) {
-        meetingCore.inviteParticipant(id: _meetingID, guest: friend)
+        inviteParticipantCancellable?.cancel()
+        
+        inviteParticipantCancellable = meetingCore.inviteParticipant(id: _meetingID, guest: friend)
             .receive(on: DispatchQueue.main)
             .sink { completion in
                 // TODO: 에러 핸들링
             } receiveValue: { [weak self] _ in
                 self?.isFloaterPresented = true
             }
-            .cancel()
     }
     
     func setMeeting(id: UInt64) {
