613590557cd8c6813bad54629e6644f84857d782
YunSwain <destap@naver.com>
2025-05-11 23:16:57 +0900

    [Improve]: 회원가입 로직 개선

--- Where/Feature/Main/ViewModel/ContentViewModel.swift
+++ Where/Feature/Main/ViewModel/ContentViewModel.swift
@@ -32,9 +32,13 @@ final class ContentViewModel: ObservableObject {
             .receive(on: DispatchQueue.main)
             .sink { [weak self] state in
                 switch state {
-                case .loginCompleted, .registrationNeeded:
+                case .loginCompleted:
                     self?.fullScreenCoverType = nil
                     self?.isLoginNeeded = false
+                    
+                case .registrationNeeded:
+                    self?.fullScreenCoverType = nil
+                    self?.isLoginNeeded = true
                     
                 case .loginNeeded:
                     self?.isLoginNeeded = true
