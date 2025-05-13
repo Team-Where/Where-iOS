3e27c271dc058eb6d75a996b5450d0f3af74b6c0
YunSwain <destap@naver.com>
2025-05-14 00:21:40 +0900

    [Feat]: 카카오톡 공유하기, 피드템플릿 설정 객체 추가

---
+++ Where/Service/Core/KakaoShareService.swift
@@ -0,0 +1,60 @@
+//
+//  KakaoShareService.swift
+//  Where
+//
+//  Created by Swain Yun on 5/13/25.
+//
+
+import Foundation
+import Combine
+import KakaoSDKCommon
+import KakaoSDKShare
+import KakaoSDKTemplate
+
+protocol KakaoShareServiceProtocol {
+    func share(inviter: User, meeting: Meeting) -> AnyPublisher<URL, MeetingCoreError>
+}
+
+final class KakaoShareService {
+    private let inviterNameKey = "USER"
+    private let meetingTitleKey = "NAME"
+    private let inviteCodeKey = "INVITE_CODE"
+    private let templateID: Int64 = 119538
+    
+    private func makeArgs(_ nickname: String, _ title: String, _ code: String) -> [String: String] {
+        [
+            inviterNameKey: nickname,
+            meetingTitleKey: title,
+            inviteCodeKey: code
+        ]
+    }
+}
+
+// MARK: - KakaoShareServiceProtocol Conformation
+extension KakaoShareService: KakaoShareServiceProtocol {
+    func share(inviter: User, meeting: Meeting) -> AnyPublisher<URL, MeetingCoreError> {
+        guard let nickname = inviter.nickname,
+              let shareLink = meeting.shareLink,
+              let code = shareLink.pathComponents.last
+        else { return Fail(error: .notSupported).eraseToAnyPublisher() }
+        
+        let args = makeArgs(nickname, meeting.title, code)
+        
+        return Future<URL, MeetingCoreError> { promise in
+            guard ShareApi.isKakaoTalkSharingAvailable() else {
+                guard let url = ShareApi.shared.makeCustomUrl(templateId: self.templateID) else {
+                    return promise(.failure(.notSupported))
+                }
+                return promise(.success(url))
+            }
+            
+            ShareApi.shared.shareCustom(templateId: self.templateID, templateArgs: args) { result, error in
+                if let error = error { promise(.failure(.networkingError(error))) }
+                if let result = result {
+                    promise(.success(result.url))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
