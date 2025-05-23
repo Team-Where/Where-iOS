09b00b32a90d5fed0b986743475f9d7a041b2839
YunSwain <destap@naver.com>
2025-05-24 00:30:59 +0900

    [Improve]: 애플로그인 API 연결 전 임시 코드 추가

--- Where/Service/Core/Authentification/AuthentificationCore.swift
+++ Where/Service/Core/Authentification/AuthentificationCore.swift
@@ -209,6 +209,11 @@ extension AuthentificationCore: AuthentificationCoreProtocol {
     }
     
     func loginWithApple(auth: ASAuthorization) {
+        // TODO: 애플로그인 로직 구현하기
+        guard let credential = auth.credential as? ASAuthorizationAppleIDCredential,
+              let authCode = credential.authorizationCode
+        else { return }
+        
         strategyContext.login(by: .apple(auth: auth)) { result in
             // TODO: WIP
         }
