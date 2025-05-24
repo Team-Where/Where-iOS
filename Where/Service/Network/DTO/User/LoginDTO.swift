644afe3db4c3c5fb2a0bc94451fc0d3b56978a77
bamsak <qjatjrdl963@naver.com>
2025-05-24 22:37:10 +0900

    [Refactor] DTO 프로퍼티 수정으로 CodingKey추가

--- Where/Service/Network/DTO/User/LoginDTO.swift
+++ Where/Service/Network/DTO/User/LoginDTO.swift
@@ -9,5 +9,10 @@ enum LoginDTO {
     struct Request: Encodable {
         let email: String
         let password: String
+        
+        enum CodingKeys: String, CodingKey {
+            case email = "username"
+            case password = "password"
+        }
     }
 }
