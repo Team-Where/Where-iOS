d7cbdcb205a219632c5c58a31a2bfbd46a67ce6b
bamsak <qjatjrdl963@naver.com>
2025-05-25 21:49:10 +0900

    [Refactor] 애플 로그인 Combine방식으로 변경

--- Where/Service/Core/Authentification/Strategy/Implementation/AppleLoginStrategy.swift
+++ Where/Service/Core/Authentification/Strategy/Implementation/AppleLoginStrategy.swift
@@ -15,6 +15,25 @@ final class AppleLoginStrategy {
 
 // MARK: - AppleLoginStrategyProtocol Confirmation
 extension AppleLoginStrategy: AuthentificationStrategyProtocol {
+    func login(provider: AuthentificationProvider) -> AnyPublisher<UserCredential, AuthentificationCoreError> {
+        guard case .apple(let auth) = provider else {
+            return Fail(error: .notSupported)
+                .eraseToAnyPublisher()
+        }
+        switch auth.credential {
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            let code = appleIDCredential.authorizationCode
+            let credential = UserCredential(authorizationCode: code)
+            return Just(credential)
+                .setFailureType(to: AuthentificationCoreError.self)
+                .eraseToAnyPublisher()
+        default:
+            return Fail(error: .socialAuthProviderAuthorizationFailed)
+                .eraseToAnyPublisher()
+        }
+    }
+    
+    
     func login(provider: AuthentificationProvider, completion: @escaping (Result<UserCredential, AuthentificationCoreError>) -> Void) {
         guard case .apple(let auth) = provider else { return completion(.failure(.notSupported)) }
 
