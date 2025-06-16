5a7405d3e7b5c432c74be5adc462600c2e2796af
bamsak <qjatjrdl963@naver.com>
2025-06-17 00:02:20 +0900

    [Feat] SharePlaceMeetingListView생성

---
+++ Where/Feature/Meeting/Share/SharePlaceMeetingListView.swift
@@ -0,0 +1,18 @@
+//
+//  SharePlaceMeetingListView.swift
+//  Where
+//
+//  Created by BOMBSGIE on 6/16/25.
+//
+
+import SwiftUI
+
+struct SharePlaceMeetingListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SharePlaceMeetingListView()
+}
