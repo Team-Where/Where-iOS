42a4f051d4af82d11ca98308f9ebc6d6c89f2bbb
bamsak <qjatjrdl963@naver.com>
2025-06-17 22:41:27 +0900

    [Feat] SharePlaceMeetingListViewModels 1차 구현
    - 현재 모임 리스트 구독

--- Where/Feature/Meeting/Share/ViewModel/SharePlaceMeetingListViewModel.swift
+++ Where/Feature/Meeting/Share/ViewModel/SharePlaceMeetingListViewModel.swift
@@ -10,5 +10,24 @@ import Swinject
 
 @Observable
 final class SharePlaceMeetingListViewModel {
+    private(set) var meetings = [Meeting]()
     
+    private let meetingCore: MeetingCoreProtocol
+    private let cancellableBag = CancellableBag()
+    
+    init(ressover: Resolver) {
+        self.meetingCore = ressover.resolve(MeetingCoreProtocol.self)!
+        subscribe()
+    }
+    
+    private func subscribe() {
+        meetingCore.meetings
+            .map {
+                $0.values.sorted { $0.createdAt > $1.createdAt }
+            }
+            .sink { [weak self] in
+                self?.meetings = $0
+            }
+            .store(in: cancellableBag, key: #function)
+    }
 }
