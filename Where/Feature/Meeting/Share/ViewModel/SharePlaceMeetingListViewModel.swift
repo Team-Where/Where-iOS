b315e3cdb289559a0968ca27761bef810e29bd59
bamsak <qjatjrdl963@naver.com>
2025-06-17 22:29:24 +0900

    [Chore] SharePlaceMeetingListView 폴더링 변경

---
+++ Where/Feature/Meeting/Share/ViewModel/SharePlaceMeetingListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SharePlaceMeetingListViewModel.swift
+//  Where
+//
+//  Created by BOMBSGIE on 6/17/25.
+//
+
+import Foundation
+import Swinject
+
+@Observable
+final class SharePlaceMeetingListViewModel {
+    
+}
