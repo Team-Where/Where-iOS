bdd5a555ff22288ba80553ce53812e3ad02ae179
bamsak <qjatjrdl963@naver.com>
2025-06-22 20:55:18 +0900

    [Feat] Comment관련 뷰모델 기능 구현

--- Where/Feature/Meeting/Place/Detail/ViewModel/PlaceDetailViewModel.swift
+++ Where/Feature/Meeting/Place/Detail/ViewModel/PlaceDetailViewModel.swift
@@ -9,28 +9,25 @@ import Foundation
 import Combine
 import Swinject
 
-@MainActor
 @Observable
 final class PlaceDetailViewModel {
-    private(set) var commentCount: Int = .zero
     private(set) var isDeletionProcessing: Bool = false
     private(set) var isTogglingProcessing: Bool = false
     
     private let placeCore: PlaceCoreProtocol
     private let cancellableBag = CancellableBag()
     
-    init(resolver: Resolver) {
-        self.placeCore = resolver.resolve(PlaceCoreProtocol.self)!
-        subscribe()
+    private var _comments = [UInt64: Comment]()
+    private var placeID: UInt64?
+    private var selectedComment: Comment?
+    
+    
+    var comments: [Comment] {
+        _comments.values.sorted { $0.createdAt < $1.createdAt }
     }
     
-    private func subscribe() {
-        placeCore.comments
-            .receive(on: DispatchQueue.main)
-            .sink { [weak self] dict in
-                self?.commentCount = dict.count
-            }
-            .store(in: cancellableBag, key: "Comments")
+    init(resolver: Resolver) {
+        self.placeCore = resolver.resolve(PlaceCoreProtocol.self)!
     }
 }
 
@@ -51,4 +48,73 @@ extension PlaceDetailViewModel {
                 self?.isTogglingProcessing = false
             } receiveValue: { _ in }
     }
+    
+    func onApear(_ placeID: UInt64) {
+        self.placeID = placeID
+        placeCore.readSpecificPlace(id: placeID)
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                //TODO: error handling
+            } receiveValue: { [weak self] in
+                self?._comments = $0
+            }
+            .store(in: cancellableBag, key: "Comments")
+
+    }
 }
+
+
+// MARK: - CommentViewModelType
+
+protocol CommentViewModelType {
+    var comments: [Comment] { get }
+    func createComment(_ description: String)
+    func updateComment(_ descrition: String)
+    func deleteComment()
+    func setComment(_ comment: Comment)
+}
+
+extension PlaceDetailViewModel: CommentViewModelType {
+    func createComment(_ description: String) {
+        guard let placeID else { return }
+        placeCore.createComment(placeID: placeID, description: description)
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                //TODO: Error handling
+            } receiveValue: { [weak self] in
+                self?._comments[$0.id] = $0
+            }
+            .store(in: cancellableBag, key: #function)
+    }
+    
+    func updateComment(_ descrition: String) {
+        guard let comment = selectedComment else { return }
+        placeCore.updateComment(comment: comment, description: descrition)
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                //TODO: Error handling
+            } receiveValue: { [weak self] newComment in
+                self?._comments[comment.id] = newComment
+            }
+            .store(in: cancellableBag, key: #function)
+        
+    }
+    
+    func deleteComment() {
+        guard let comment = selectedComment else { return }
+        placeCore.deleteComment(comment: comment)
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                //TODO: Error handling
+            } receiveValue: { [weak self] in
+                self?._comments.removeValue(forKey: $0)
+            }
+            .store(in: cancellableBag, key: #function)
+    }
+    
+    func setComment(_ comment: Comment) {
+        self.selectedComment = comment
+    }
+}
+
+
