3c1e053a8f24ac829dde7ef37990632267075d03
bamsak <qjatjrdl963@naver.com>
2025-07-07 23:18:17 +0900

    [Feat] PayloadType의 기본 프로퍼티를 같는 구조체 구현 다른 Payload를 생성 시 사용

--- Where/Service/Notifications/DTO/PayloadType.swift
+++ Where/Service/Notifications/DTO/PayloadType.swift
@@ -11,6 +11,38 @@ protocol PayloadType {
     var id: UInt64 { get }
     var title: String { get }
     var content: String { get }
+    var type: NotificationType { get }
     
     init?(userInfo: [AnyHashable : Any])
+}
+
+
+struct BasicPayload: PayloadType {
+    let id: UInt64
+    let title: String
+    let content: String
+    let type: NotificationType
+    
+    init?(userInfo: [AnyHashable : Any]) {
+        guard let id = userInfo["gcm.message_id"] as? UInt64
+        else { return nil }
+        
+        guard let aps   = userInfo["aps"] as? [String: Any],
+              let alert = aps["alert"] as? [String: Any],
+              let title = alert["title"] as? String,
+              let body  = alert["body"] as? String
+        else {
+            return nil
+        }
+        
+        guard let code = userInfo["code"] as? Int,
+              let type = NotificationType(rawValue: code)
+        else {
+            return nil
+        }
+        self.id = id
+        self.title = title
+        self.content = body
+        self.type = type
+    }
 }
