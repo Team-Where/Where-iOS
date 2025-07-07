89827fd3f31e12daa1dcde67448b7c0923ea7cc2
bamsak <qjatjrdl963@naver.com>
2025-07-07 23:56:20 +0900

    [Refactor] 장소 선정 알림 PayloadType 채택 및 이니셜라이저 구현

--- Where/Service/Notifications/DTO/PlacePickedPayload.swift
+++ Where/Service/Notifications/DTO/PlacePickedPayload.swift
@@ -7,8 +7,30 @@
 
 import Foundation
 
-struct PlacePickedPayload {
+struct PlacePickedPayload: PayloadType {
+    let id: UInt64
+    let title: String
+    let content: String
+    let type: NotificationType
     
     let placeID: UInt64
     let isPicked: Bool
+    
+    init?(userInfo: [AnyHashable : Any]) {
+        guard let basicPayload = BasicPayload(userInfo: userInfo) else { return nil }
+        
+        guard let placeID = userInfo[UserInfoKey.placeId.rawValue] as? UInt64,
+              let isPicked = userInfo[UserInfoKey.placeStatus.rawValue] as? String
+        else {
+            return nil
+        }
+        
+        self.id = basicPayload.id
+        self.title = basicPayload.title
+        self.content = basicPayload.content
+        self.type = basicPayload.type
+        
+        self.placeID = placeID
+        self.isPicked = isPicked == "Picked"
+    }
 }
