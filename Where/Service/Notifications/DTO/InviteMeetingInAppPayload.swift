2bd70e38adf409a12ce06c5c8d091aefb2443d8e
bamsak <qjatjrdl963@naver.com>
2025-07-07 23:19:24 +0900

    [Feat] 앱 내 모임 초대 시 알림 파싱 구현

--- Where/Service/Notifications/DTO/InviteMeetingInAppPayload.swift
+++ Where/Service/Notifications/DTO/InviteMeetingInAppPayload.swift
@@ -7,7 +7,11 @@
 
 import Foundation
 
-struct InviteMeetingInAppPayload {
+struct InviteMeetingInAppPayload: PayloadType {
+    let id: UInt64
+    let title: String
+    let content: String
+    let type: NotificationType
     
     let inviteID: UInt64
     let meetingID: UInt64
@@ -16,4 +20,35 @@ struct InviteMeetingInAppPayload {
     let meetingTitle: String
     let scheduleDate: String?
     let scheduleTime: String?
+    
+    init?(userInfo: [AnyHashable : Any]) {
+        guard let basicPayload = BasicPayload(userInfo: userInfo)
+        else {
+            return nil
+        }
+        guard let inviteID  = userInfo["inviteId"] as? UInt64,
+              let meetingID = userInfo["meetingId"] as? UInt64
+        else {
+            return nil
+        }
+        
+        guard let hostNickname = userInfo["fromNickName"] as? String,
+              let meetingTitle = userInfo["meetingTitle"] as? String
+        else {
+            return nil
+        }
+        self.id = basicPayload.id
+        self.title = basicPayload.title
+        self.content = basicPayload.content
+        self.type = basicPayload.type
+        
+        
+        self.inviteID = inviteID
+        self.meetingID = meetingID
+        self.meetingImage = userInfo["meetingImage"] as? String
+        self.hostNickname = hostNickname
+        self.meetingTitle = meetingTitle
+        self.scheduleDate = userInfo["scheduleDate"] as? String
+        self.scheduleTime = userInfo["scheduleTime"] as? String
+    }
 }
