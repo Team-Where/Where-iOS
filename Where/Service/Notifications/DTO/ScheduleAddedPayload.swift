0fcf32fcdd420972332ce34ab358e8d8ace2d6a7
bamsak <qjatjrdl963@naver.com>
2025-07-08 01:36:01 +0900

    [Refactor] 스케줄 관련 알림 파싱 Payload타입 채택 및 이니셜라이저 구현

--- Where/Service/Notifications/DTO/ScheduleAddedPayload.swift
+++ Where/Service/Notifications/DTO/ScheduleAddedPayload.swift
@@ -7,9 +7,22 @@
 
 import Foundation
 
-struct ScheduleAddedPayload {
-    
+struct ScheduleAddedPayload: PayloadType {
     let meetingID: UInt64
     let date: String
     let time: String
+    
+    let _base: BasePayload
+    
+    init?(userInfo: [AnyHashable : Any]) {
+        guard let basePayload = BasePayload(userInfo: userInfo) else { return nil }
+        guard let meetingID = userInfo[UserInfoKey.meetingId.rawValue] as? UInt64,
+              let date = userInfo[UserInfoKey.date.rawValue] as? String,
+              let time = userInfo[UserInfoKey.time.rawValue] as? String
+        else { return nil }
+        self.meetingID = meetingID
+        self.date = date
+        self.time = time
+        self._base = basePayload
+    }
 }
--- Where/Service/Notifications/DTO/ScheduleDeletedPayload.swift
+++ Where/Service/Notifications/DTO/ScheduleDeletedPayload.swift
@@ -7,7 +7,15 @@
 
 import Foundation
 
-struct ScheduleDeletedPayload {
-    
+struct ScheduleDeletedPayload: PayloadType {
     let meetingID: UInt64
+    let _base: BasePayload
+    
+    init?(userInfo: [AnyHashable : Any]) {
+        guard let basePayload = BasePayload(userInfo: userInfo) else { return nil }
+        guard let meetingID = userInfo[UserInfoKey.meetingId.rawValue] as? UInt64 else { return nil }
+        
+        self.meetingID = meetingID
+        self._base = basePayload
+    }
 }
