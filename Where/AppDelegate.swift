e9ca68f06d549ea694431846481e7d88ecad8c6f
YunSwain <destap@naver.com>
2025-08-07 21:51:25 +0900

    [Refactor]: Foreground 동작 중 알림 수신으로 인해 초대수락화면으로 갑자기 이동되어 UX 저하되지 않도록 변경

--- Where/AppDelegate.swift
+++ Where/AppDelegate.swift
@@ -70,10 +70,7 @@ extension AppDelegate: UIApplicationDelegate {
 extension AppDelegate: UNUserNotificationCenterDelegate {
     /// Foreground에서 푸시 알림을 수신했을 때 호출됨
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
-        let userInfo = notification.request.content.userInfo
-        let shouldDisplayNotifications = UserDefaults.standard.bool(forKey: AppStorageKey.shouldDisplayNotifications)
-        notificationCore.handleReceivedNotificationPayload(userInfo)
-        return shouldDisplayNotifications ? [.banner, .badge, .sound] : []
+        return [.banner, .sound, .list, .badge]
     }
     
     /// Foreground 또는 Background에서 푸시 수신 후, 사용자가 터치하여 앱을 열었을 때 호출됨
